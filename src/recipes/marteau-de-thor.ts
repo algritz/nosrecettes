cbfe606de63b17873f892ba71c5a987d82f99c7b
David Cloutier <sheldreyn@gmail.com>
2025-09-09 20:54:19 -0400

    Add recipe: Marteau de Thor

---
+++ src/recipes/marteau-de-thor.ts
@@ -0,0 +1,25 @@
+import { Recipe } from '@/types/recipe';
+
+export const marteaudethorRecipe: Recipe = {
+  id: '1757465658768',
+  title: 'Marteau de Thor',
+  description: 'Impressionnez vos invités avec cette spectaculaire recette de jarret de boeuf',
+  category: 'Fumoir',
+  prepTime: 20,
+  cookTime: 720,
+  marinatingTime: 480,
+  servings: 12,
+  difficulty: 'Moyen',
+  ingredients: [
+
+  ],
+  instructions: [
+
+  ],
+  tags: [],
+  image: '/images/marteau-de-thor.jpg',
+  accompaniment: 'Saucisses fumées',
+  wine: 'Baron de Ley, reserva',
+  source: 'David Cloutier',
+  slug: 'marteau-de-thor'
+};
