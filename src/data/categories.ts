ded84f28a3c843c3f7c9a331440c0a0d37a33cef
David Cloutier <sheldreyn@gmail.com>
2025-09-25 08:32:08 -0400

    Update categories: +29 -1

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,12 +1,40 @@
 // Default recipe categories
 export const recipeCategories = [
   'Accompagnements',
-  'Collations',
+  'Agneau',
+  'Amuse-geules',
+  'Barbecue',
+  'Boeuf',
+  'Condiments',
+  'Cuisson sous-vide',
   'Desserts',
+  'Déjeuners',
   'Entrées',
+  'Fruits',
+  'Fruits de mer',
+  'Fumoir',
+  'Gibier à plumes',
+  'Gibier à poils',
+  'Légumes',
+  'Marinade',
+  'Mijoteuse',
+  'Pain',
+  'Patates',
   'Plats principaux',
+  'Poisson',
+  'Porc',
+  'Pâtes',
+  'Pâtisseries et desserts',
+  'Pâtés',
   'Salades',
-  'Soupes'
+  'Sandwichs',
+  'Sauces',
+  'Soupes',
+  'Tartares',
+  'Trempettes',
+  'Veau',
+  'Vollaille',
+  'Végétarien'
 ];
 
 // Helper function to get all categories (from recipes + defaults)
