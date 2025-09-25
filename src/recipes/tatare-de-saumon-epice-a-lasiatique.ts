4be4cfe756ce80af8d78877fbf098db2bf7cac62
David Cloutier <sheldreyn@gmail.com>
2025-09-25 14:28:36 -0400

    Add recipe: Tatare de saumon épicé à l'asiatique

---
+++ src/recipes/tatare-de-saumon-epice-a-lasiatique.ts
@@ -0,0 +1,38 @@
+import { Recipe } from '@/types/recipe';
+
+export const tatareDeSaumonEpiceALasiatique: Recipe = {
+  id: '1758824915482',
+  title: 'Tatare de saumon épicé à l\'asiatique',
+  description: '',
+  categories: ['Plats principaux', 'Tartares'],
+  prepTime: 25,
+  cookTime: 0,
+  servings: 2,
+  difficulty: 'Facile',
+  ingredients: [
+    '175 grammes de filet de saumon très frais, sans la peau, taillé en petits dés d\'environ 0.5 centimètres',
+    '1 oignon vert, finement haché',
+    '1 cuillère à thé de gingembre, finement émincé',
+    '1 cuillère à thé de galanga, finement émincé (si vous n\'en avez pas, doublez la quantité de gingembre)',
+    '1 petite gousse d\'ail, finement émincé',
+    '1 pincée de grains de coriandre, moulus au mortier et au pilon',
+    '1 pincée de poivre entier, moulus au mortier et au pilon Coriandre fraîche, hachée finement',
+    'Le jus de 2 lime ',
+    'Huile de sésame grillé (un filet)',
+    '1 petit cuillère à thé de mayonnaise ou de Waffu',
+    '1 petit trait de sauce Sriracha',
+    '1 petit piment thaï séché, écrasé en flocons',
+    'Vinaigre de riz (un filet)'
+  ],
+  instructions: [
+    'Tailler le saumon en dés puis réserver au froid.',
+    'Préparer tous les autres ingrédients et réserver.',
+    'Le tartare doit être monté juste au moment de servir.',
+    'Lorsque vous êtes prêts à servir, mélanger le saumon, le gingembre, le galanga, l\'ail, la coriandre et le poivre.',
+    'Ajouter le piment, la Sriracha, puis l\'assaisonnement liquide (il faut vraiment avoir la main légère, pour ne pas trop en mettre): d\'abord le jus de lime, puis le vinaigre de riz, et adoucissez avec l\'huile et la mayonnaise ou le Waffu, si vous en avez.',
+    'Bien mélanger, goûter et rectifier l\'assaisonnement au besoin.'
+  ],
+  tags: ['saumon', 'poisson'],
+  source: 'David Cloutier',
+  slug: 'tatare-de-saumon-epice-a-lasiatique'
+};
