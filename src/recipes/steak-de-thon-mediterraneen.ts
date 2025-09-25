9d3a5183926a52dd3d0528486812931ba19a5655
David Cloutier <sheldreyn@gmail.com>
2025-09-25 11:18:25 -0400

    Add recipe: Steak de thon méditerranéen

---
+++ src/recipes/steak-de-thon-mediterraneen.ts
@@ -0,0 +1,31 @@
+import { Recipe } from '@/types/recipe';
+
+export const steakDeThonMediterraneen: Recipe = {
+  id: '1758813505090',
+  title: 'Steak de thon méditerranéen',
+  description: '',
+  categories: ['Plats principaux', 'Poisson', 'Barbecue'],
+  prepTime: 15,
+  cookTime: 10,
+  marinatingTime: 45,
+  servings: 2,
+  difficulty: 'Facile',
+  ingredients: [
+    '2 à 4 steak de thon',
+    '4 de tasse huile d\'olive',
+    'le jus d\'un demi citron',
+    '1 c. à thé cumin en graine',
+    '1 morceau de gingembre d\'un pouce haché finement',
+    '1 gousse d\' haché finement',
+    '1 c. à soupe ou plus de coriandre haché Une pincée safran sel et poivre'
+  ],
+  instructions: [
+    'Moudre le cumin et mettre dans un bol.',
+    'Moudre le safran et ajouter le jus de citron au safran, laisser reposer 2-3 minutes et verser dans le même bol que le cumin moulu Ajouter dans ce bol le reste des ingrédients et y faire mariner le poisson 20 à 30 minutes.',
+    'Cuire pas trop longtemps sur le grill.'
+  ],
+  tags: ['thon', 'poisson'],
+  accompaniment: 'Salade Grècque',
+  source: 'David Cloutier',
+  slug: 'steak-de-thon-mediterraneen'
+};
