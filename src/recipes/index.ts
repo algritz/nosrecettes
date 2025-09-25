f1c385196426c05d77da2bb1e00511affadd918a
David Cloutier <sheldreyn@gmail.com>
2025-09-25 10:04:33 -0400

    Remove recipe from index: Tarte au Sucre

--- src/recipes/index.ts
+++ src/recipes/index.ts
@@ -1,13 +1,11 @@
 import { Recipe } from '@/types/recipe';
 import { poutineClassique } from './poutine-classique';
 import { tourtiereDuLacSaintJean } from './tourtiere-du-lac-saint-jean';
-import { tarteAuSucre } from './tarte-au-sucre';
 import { tartareDeThonRadisEtPeches } from './tartare-de-thon-radis-et-peches';
 import { brigadeiroBlanc } from './brigadeiro-blanc';
 
 export const recipes: Recipe[] = [poutineClassique,
-  tourtiereDuLacSaintJean,
-  tarteAuSucre,
+  tourtiereDuLacSaintJean
   tartareDeThonRadisEtPeches,
   brigadeiroBlanc];
 
