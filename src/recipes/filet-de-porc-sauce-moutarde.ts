5b5d6762c61ebed550b2eea6ebef084ce7809532
David Cloutier <sheldreyn@gmail.com>
2025-10-02 17:06:47 -0400

    Add recipe: Filet de porc sauce moutarde (#37)

---
+++ src/recipes/filet-de-porc-sauce-moutarde.ts
@@ -0,0 +1,31 @@
+import { Recipe } from '@/types/recipe';
+
+export const filetDePorcSauceMoutarde: Recipe = {
+  id: '1759439126921',
+  title: 'Filet de porc sauce moutarde',
+  description: '',
+  categories: ['Porc'],
+  prepTime: 5,
+  cookTime: 15,
+  servings: 1,
+  difficulty: 'Facile',
+  ingredients: [
+    'beurre 15 ml (1 c. à soupe)',
+    'Huile-15 ml (1 c. à soupe)',
+    'Filet de porc, coupé en tranches',
+    'Vin blanc sec -200 ml (⅘ tasse)',
+    'Crème 15 %-150 ml (⅔ tasse)',
+    'Moutarde à l\'ancienne-30 ml (2 c. à soupe)',
+    'Poivre-Au goût'
+  ],
+  instructions: [
+    'Chauffez le beurre et l’huile dans un poêlon. Lorsque le beurre est mousseux, saisissez la viande 2 à 3 minutes de chaque côté.',
+    'Réduisez le feu et faites cuire 4 à 5 minutes. Réservez. Déposez la viande dans une assiette de service et gardez au chaud.',
+    'À feu vif, versez le vin dans le poêlon en déglaçant le fond avec une cuillère de bois. Faites bouillir jusqu’à ce que le vin soit réduit à environ 45 ml (3 c. à soupe).',
+    'Ajoutez la crème, la moutarde et le poivre; mélangez. Faites bouillir légèrement.',
+    'Versez la sauce sur le porc et servez accompagné de légumes de saison.'
+  ],
+  tags: ['rapide', 'simple'],
+  source: 'https://ici.radio-canada.ca/mordu/recettes/1706/filet-porc-sauce-moutarde',
+  slug: 'filet-de-porc-sauce-moutarde'
+};
