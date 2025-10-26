a43b311bf5dd371b0a7a0fedf0646c5ceee0be30
David Cloutier <sheldreyn@gmail.com>
2025-10-26 11:39:12 -0400

    Add recipe: Marinade thaïe au lait de coco (#48)

---
+++ src/recipes/marinade-thaie-au-lait-de-coco.ts
@@ -0,0 +1,27 @@
+import { Recipe } from '@/types/recipe';
+
+export const marinadeThaieAuLaitDeCoco: Recipe = {
+  id: '1761493043147',
+  title: 'Marinade thaïe au lait de coco',
+  description: 'Marinade express au lait de coco, beurre d\'arachide, sauce soya, lime et sambal oelek.',
+  categories: ['Marinade'],
+  prepTime: 5,
+  cookTime: 0,
+  marinatingTime: 240,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '250 ml (1 tasse) de lait de coco',
+    '125 ml (1/2 tasse) de beurre d\'arachide',
+    '30 ml (2 c. à soupe) de sauce soya',
+    'Le zeste et le jus d\'une lime',
+    '5 ml (1 c. à thé) de sambal oelek'
+  ],
+  instructions: [
+    'Dans un bol, mélanger tous les ingrédients à l\'aide d\'un fouet.',
+    'Ajouter la viande et laisser mariner.'
+  ],
+  tags: ['lait de coco', 'sambal oelek', 'lime'],
+  source: 'David Cloutier',
+  slug: 'marinade-thaie-au-lait-de-coco'
+};
