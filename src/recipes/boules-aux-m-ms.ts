36439faee3ba74117e5c40b27f2b4b659e399a3e
David Cloutier <sheldreyn@gmail.com>
2025-11-30 14:47:28 -0500

    Add recipe: Boules aux M & M's (#62)

---
+++ src/recipes/boules-aux-m-ms.ts
@@ -0,0 +1,31 @@
+import { Recipe } from '@/types/recipe';
+
+export const boulesAuxMMs: Recipe = {
+  id: '1764532005479',
+  title: 'Boules aux M & M\'s',
+  description: 'Boules de beurre d\'arachide enrobées de chocolat, garnies d\'un M&M.',
+  categories: ['Pâtisseries et desserts', 'Desserts'],
+  prepTime: 15,
+  cookTime: 10,
+  servings: 0,
+  difficulty: 'Facile',
+  ingredients: [
+    '1/2 tasse de beurre fondu',
+    '1 1/2 tasses de beurre d\'arachide',
+    '2 1/2 tasses de sucre en poudre',
+    '2 tasses de pépites de chocolat',
+    '1 sac de M & M\'s au chocolat',
+    '2 c. à soupe de beurre'
+  ],
+  instructions: [
+    'Dans un bol, mélanger le beurre fondu, le beurre d\'arachide, les M & M\'s et le sucre jusqu\'à consistance homogène.',
+    'Réfrigérer 30 minutes.',
+    'Façonner de petites boules et les déposer sur une plaque à biscuits tapissée de papier parchemin. Remettre au frais.',
+    'Faire fondre les pépites de chocolat au bain-marie avec les 2 c. à soupe de beurre, en remuant jusqu\'à ce que le mélange soit lisse.',
+    'Tremper chaque boule dans le chocolat fondu et redéposer sur la plaque. Ajouter un M & M sur le dessus de chaque boule.',
+    'Réfrigérer jusqu’à prise.'
+  ],
+  tags: ['beurre d\'arachide', 'M & M\'s', 'enrobage chocolat'],
+  source: 'David Cloutier',
+  slug: 'boules-aux-m-ms'
+};
