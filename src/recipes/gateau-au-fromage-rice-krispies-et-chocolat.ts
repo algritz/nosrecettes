c1e6d543dc9fbf849738b1175ca0adb4bfcd8099
David Cloutier <sheldreyn@gmail.com>
2025-11-30 15:14:55 -0500

    Add recipe: Gâteau au fromage rice Krispies et chocolat (#68)

---
+++ src/recipes/gateau-au-fromage-rice-krispies-et-chocolat.ts
@@ -0,0 +1,38 @@
+import { Recipe } from '@/types/recipe';
+
+export const gateauAuFromageRiceKrispiesEtChocolat: Recipe = {
+  id: '1764533624954',
+  title: 'Gâteau au fromage rice Krispies et chocolat',
+  description: 'Cheesecake sans croûte classique: base de Rice Krispies-guimauves, appareil chocolat-fromage à la crème, décor fruits/chocolat.',
+  categories: ['Pâtisseries et desserts', 'Desserts'],
+  prepTime: 20,
+  cookTime: 10,
+  marinatingTime: 180,
+  servings: 10,
+  difficulty: 'Facile',
+  ingredients: [
+    '1/4 tasse de beurre non salé',
+    '10 oz (285 g) de mini guimauves',
+    '6 tasses de céréales Rice Krispies',
+    '8 oz (225 g) de chocolat mi‑sucré',
+    '1/4 tasse (60 ml) de crème épaisse',
+    '16 oz (455 g) de fromage à la crème, ramolli',
+    '1/2 tasse (100 g) de sucre',
+    '1 c. à thé de vanille',
+    '2 1/2 tasses (150 g) de crème fouettée',
+    'Barre de chocolat et/ou petits fruits des champs frais, pour décoration'
+  ],
+  instructions: [
+    'Dans une grande casserole à feu moyen, faire fondre le beurre. Ajouter les guimauves et remuer jusqu’à ce qu’elles soient fondues.',
+    'Ajouter les Rice Krispies et remuer pour bien enrober. Transférer immédiatement dans un moule à charnière graissé de 10 po (25 cm). À l’aide d’une tasse à mesurer graissée, presser pour former le fond et les bords. Réserver.',
+    'Mettre le chocolat dans un bol allant au micro‑ondes et verser la crème épaisse. Faire fondre par intervalles de 30 secondes en remuant, jusqu’à homogénéité (environ 1 minute).',
+    'Dans un grand bol, battre le fromage à la crème, le sucre et la vanille jusqu’à lisse. Incorporer le chocolat fondu.',
+    'Ajouter 1 1/2 tasse (90 g) de crème fouettée et plier à la spatule jusqu’à homogénéité.',
+    'Verser l’appareil au fromage sur la base de riz croustillant et lisser. Réfrigérer 3 à 4 heures ou toute la nuit, jusqu’à prise.',
+    'Démouler. Mettre la crème fouettée restante dans une poche à douille (pointe étoile) et décorer le dessus.',
+    'Râper du chocolat au milieu du gâteau et/ou garnir de petits fruits. Servir.'
+  ],
+  tags: ['rice krispies', 'cheesecake', 'chocolat'],
+  source: 'David Cloutier',
+  slug: 'gateau-au-fromage-rice-krispies-et-chocolat'
+};
