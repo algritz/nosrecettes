efd4cc6886a1bd4d0704cf2db9c167cba4147a8a
David Cloutier <sheldreyn@gmail.com>
2025-12-23 11:01:14 -0500

    Add recipe: Saumon grillé sur une planche de cèdre

---
+++ src/recipes/saumon-grille-sur-une-planche-de-cedre.ts
@@ -0,0 +1,32 @@
+import { Recipe } from '@/types/recipe';
+
+export const saumonGrilleSurUnePlancheDeCedre: Recipe = {
+  id: 'saumon-grille-sur-une-planche-de-cedre',
+  title: 'Saumon grillé sur une planche de cèdre',
+  description: 'Saumon grillé sur une planche de cèdre',
+  categories: ['Végétarien'],
+  prepTime: 10,
+  cookTime: 20,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '1 planche de cèdre (environ 14 x 7 x 1 po) non traitée',
+    '1 filet de saumon (2 lb ou 900 g) de 1 po d\'épaisseur',
+    '1/2 tasse de vinaigrette Tomate confite et origan Kraft',
+    '1/4 tasse de persil frais, haché fin',
+    '1/4 tasse de tomates séchées dans l\'huile, hachées fin',
+    '1 c. à soupe d’huile végétale'
+  ],
+  instructions: [
+    'Immerger la planche dans l\'eau, en plaçant un poids sur le dessus pour la garder submergée. Laisser tremper au moins 4 heures ou jusqu\'au lendemain.',
+    'Chauffer le barbecue à feu moyen.',
+    'Mélanger la vinaigrette, le persil et les tomates; mettre de côté.',
+    'Badigeonner la planche de cèdre avec de l\'huile; placer le saumon dessus.',
+    'Mettre sur le gril; baisser le couvercle du barbecue.',
+    'Griller 10 minutes.',
+    'Badigeonner avec le mélange de vinaigrette; continuer la cuisson 10 min ou jusqu\'à ce que le saumon s\'émiette facilement à la fourchette.'
+  ],
+  tags: ['barbecue', 'fumé', 'saumon'],
+  marinatingTime: 240,
+  slug: 'saumon-grille-sur-une-planche-de-cedre'
+};
