5b52965efdb8f0e731896e3e50db738b47a917a8
David Cloutier <sheldreyn@gmail.com>
2025-12-23 12:20:00 -0500

    Add recipe: Patates douces au chutney de mangue

---
+++ src/recipes/patates-douces-au-churtney-de-mangue.ts
@@ -0,0 +1,32 @@
+import { Recipe } from '@/types/recipe';
+
+export const patatesDoucesAuChurtneyDeMangue: Recipe = {
+  id: 'patates-douces-au-churtney-de-mangue',
+  title: 'Patates douces au chutney de mangue',
+  description: '',
+  categories: ['Végétarien', 'Accompagnements'],
+  prepTime: 10,
+  cookTime: 20,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '700 g de patates douces',
+    '2 c. à soupe de sauce soya',
+    '2 c à soupe de chutney de mangue',
+    '2 limes',
+    'Coriandre hachée',
+    'huile d\'olive',
+    'Sel',
+    'piment d\'Espelette'
+  ],
+  instructions: [
+    'Laver, éplucher et cuire à la vapeur les patates douces coupées en gros morceaux avec une lime coupée en deux.',
+    'Pendant ce temps, hacher la coriandre sur une planche de travail.',
+    'Mélanger la coriandre hachée avec le chutney de mangue, la sauce soya, l\'huile d\'olive et le jus d\'une demi lime.',
+    'Déposer les patates douces cuites et chaudes sur le mélange, puis les écraser grossièrement avec une fourchette ou un pilon à patate.',
+    'Récupérer la lime qui a cuit avec les patates douces et ajouter la moitié du jus de la lime par-dessus.',
+    'Bien mélanger l\'ensemble et rectifier l\'assaisonnement selon votre goût.'
+  ],
+  tags: ['mangue', 'chutney', 'cuisson vapeur'],
+  slug: 'patates-douces-au-churtney-de-mangue'
+};
