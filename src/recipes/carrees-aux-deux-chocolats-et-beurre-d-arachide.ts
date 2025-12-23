3b836dab799940b53b8d64611026596154631e8c
David Cloutier <sheldreyn@gmail.com>
2025-12-23 10:59:54 -0500

    Add recipe: Carrées aux deux chocolats et beurre d'arachide

---
+++ src/recipes/carrees-aux-deux-chocolats-et-beurre-d-arachide.ts
@@ -0,0 +1,26 @@
+import { Recipe } from '@/types/recipe';
+
+export const carreesAuxDeuxChocolatsEtBeurreDArachide: Recipe = {
+  id: 'carrees-aux-deux-chocolats-et-beurre-d-arachide',
+  title: 'Carrées aux deux chocolats et beurre d\'arachide',
+  description: 'Carrées aux deux chocolats et beurre d\'arachide',
+  categories: ['Pâtisseries et desserts'],
+  prepTime: 15,
+  cookTime: 15,
+  servings: 24,
+  difficulty: 'Facile',
+  ingredients: [
+    '1 tasse de chocolat noir fondu',
+    '1 tasse de chocolat blanc fondu',
+    '2 tasses de beurre d\'arachide'
+  ],
+  instructions: [
+    'Faire fondre ensemble le chocolat noir et le beurre d\'arachide',
+    'Verser dans un plat carré style pyrex',
+    'Faire fondre le chocolat blanc et verser sur le premier mélange en brassant délicatement pour marbrer',
+    'Réfrigérer une nuit',
+    'Couper et déguster'
+  ],
+  tags: ['chocolat', 'marbré', 'réfrigération'],
+  slug: 'carrees-aux-deux-chocolats-et-beurre-d-arachide'
+};
