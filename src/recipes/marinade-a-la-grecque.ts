20f11935efd098f3f4c9584b32ab3246a7d32fc7
David Cloutier <sheldreyn@gmail.com>
2025-12-23 11:33:37 -0500

    Add recipe: Marinade à la grecque

---
+++ src/recipes/marinade-a-la-grecque.ts
@@ -0,0 +1,31 @@
+import { Recipe } from '@/types/recipe';
+
+export const marinadeALaGrecque: Recipe = {
+  id: 'marinade-a-la-grecque',
+  title: 'Marinade à la grecque',
+  description: 'Une marinade passe-partout Marinade à la grecque',
+  categories: ['Marinade'],
+  prepTime: 20,
+  cookTime: 0,
+  servings: 0,
+  difficulty: 'Facile',
+  ingredients: [
+    '1/2 tasse (125 ml) huile',
+    '1 1/2 c.à soupe (22 ml) jus de citron',
+    '1/2 oignon, haché finement',
+    '1 c.à thé (5 ml) moutarde',
+    '1/4 c.à thé (1 ml) épices à steak',
+    '2 feuilles laurier',
+    '1 c.à soupe (15 ml) base de poulet',
+    '2 gousses ail',
+    '1 c.à soupe (15 ml) sauce soya',
+    '1 c.à soupe (15 ml) vinaigre blanc',
+    '1/4 c.à thé (1 ml) origan'
+  ],
+  instructions: [
+    'Mêler ensemble tous les ingrédient des la marinade.',
+    'Laisser reposer le porc ou poulet dans cette marinade au réfrigérateur pendant 2 heures minimum puis faire cuire selon son choix, mais idéale sur le BBQ'
+  ],
+  tags: ['marinade sèche', 'grill'],
+  slug: 'marinade-a-la-grecque'
+};
