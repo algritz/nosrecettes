1ddce96b1d72a3c3b95aeaf7dcbc1edbda4ff44c
David Cloutier <sheldreyn@gmail.com>
2025-12-23 11:20:33 -0500

    Add recipe: Trempette étagée à Nachos

---
+++ src/recipes/trempette-etagee-a-nachos.ts
@@ -0,0 +1,26 @@
+import { Recipe } from '@/types/recipe';
+
+export const trempetteEtageeANachos: Recipe = {
+  id: 'trempette-etagee-a-nachos',
+  title: 'Trempette étagée à Nachos',
+  description: 'Simple, mais efficace Trempette étagée à Nachos',
+  categories: ['Trempettes'],
+  prepTime: 20,
+  cookTime: 0,
+  servings: 0,
+  difficulty: 'Facile',
+  ingredients: [
+    'Haricots sautés Old El Paso',
+    'Guacamole',
+    'Crème sure',
+    'Salsa',
+    'Fromage râpé',
+    'Tomates en dès',
+    'Olives noires tranchées'
+  ],
+  instructions: [
+    'Placer les ingrédients dans l\'ordre mentionné ci-haut dans un plat de pyrex.'
+  ],
+  tags: [],
+  slug: 'trempette-etagee-a-nachos'
+};
