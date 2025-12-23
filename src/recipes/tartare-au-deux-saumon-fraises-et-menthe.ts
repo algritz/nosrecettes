7d32643875db234dacf26dd414e8961cd8ca51b9
David Cloutier <sheldreyn@gmail.com>
2025-12-23 10:58:02 -0500

    Add recipe: Tartare au deux saumon, fraises et menthe

---
+++ src/recipes/tartare-au-deux-saumon-fraises-et-menthe.ts
@@ -0,0 +1,30 @@
+import { Recipe } from '@/types/recipe';
+
+export const tartareAuDeuxSaumonFraisesEtMenthe: Recipe = {
+  id: 'tartare-au-deux-saumon-fraises-et-menthe',
+  title: 'Tartare au deux saumon, fraises et menthe',
+  description: 'Tartare au deux saumon, fraises et menthe',
+  categories: ['Entrées'],
+  prepTime: 15,
+  cookTime: 0,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '200g de filet de saumon fumé à froid',
+    '300g de saumon frais',
+    '1 c. à soupe d\'oeuf de poisson volant (Tobiko)',
+    '1 tasse de fraises coupés en petits dés',
+    '8-9 feuilles de menthe grossièrement hachées',
+    '10-12 brins de ciboulette ciselés',
+    '2 c. à soupe de graines de sésame',
+    '1 c. à soupe de câpres finement hachés',
+    '1 c. à soupe de mayonnaise',
+    '1 c. à thé de moutarde de dijon'
+  ],
+  instructions: [
+    'Couper les filets de saumon en petits dés.',
+    'Y incorporer tout le reste des ingrédients et servir aussi tôt.'
+  ],
+  tags: ['cru', 'frais', 'entrée'],
+  slug: 'tartare-au-deux-saumon-fraises-et-menthe'
+};
