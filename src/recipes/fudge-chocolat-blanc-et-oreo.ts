8acdd71e7bda85c178edf3de86645493dbda6965
David Cloutier <sheldreyn@gmail.com>
2025-12-23 11:10:30 -0500

    Add recipe: Fudge chocolat blanc et Oréo

---
+++ src/recipes/fudge-chocolat-blanc-et-oreo.ts
@@ -0,0 +1,28 @@
+import { Recipe } from '@/types/recipe';
+
+export const fudgeChocolatBlancEtOreo: Recipe = {
+  id: 'fudge-chocolat-blanc-et-oreo',
+  title: 'Fudge chocolat blanc et Oréo',
+  description: 'Fudge chocolat blanc et Oréo',
+  categories: ['Pâtisseries et desserts'],
+  prepTime: 15,
+  cookTime: 10,
+  servings: 40,
+  difficulty: 'Facile',
+  ingredients: [
+    '450g de chocolat blanc',
+    '1 boite de lait Eagle Brand',
+    '1 c. à soupe de beurre',
+    '1 c. à thé de vanille',
+    '13 Oréo écrasés grossièrement'
+  ],
+  instructions: [
+    'Faire fondre le chocolat blanc au bain-marie avec le beurre',
+    'Ajouter le lait Eagle Brand et la vanille et bien mélanger',
+    'Ajouter les Oréo concassés',
+    'Bien brasser et verser dans un moule 9 x 9 en silicone',
+    'Réfrigérer 4h, démouler et couper en petits morceaux'
+  ],
+  tags: ['chocolat blanc', 'Oréo', 'fudge'],
+  slug: 'fudge-chocolat-blanc-et-oreo'
+};
