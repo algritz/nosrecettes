2f65636094e41d6e0804389755b915e63cc907eb
David Cloutier <sheldreyn@gmail.com>
2025-12-23 11:24:09 -0500

    Add recipe: Fondu au fromage pas ordinaire

---
+++ src/recipes/fondu-au-fromage-pas-ordinaire.ts
@@ -0,0 +1,34 @@
+import { Recipe } from '@/types/recipe';
+
+export const fonduAuFromagePasOrdinaire: Recipe = {
+  id: 'fondu-au-fromage-pas-ordinaire',
+  title: 'Fondu au fromage pas ordinaire',
+  description: 'Fondu au fromage pas ordinaire',
+  categories: ['Végétarien', 'Plats principaux'],
+  prepTime: 15,
+  cookTime: 20,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '1 piment vert en petits dés',
+    '3 tasses de poireaux tranchés finement',
+    '1 gousse d’ail',
+    '250 ml de bière blonde au choix',
+    '45 ml de beurre',
+    '1 tomate épépinée et coupée en dés',
+    '15 ml d’épices italiennes',
+    '15 ml de fécule de maïs',
+    '350 g de fromage emmental râpé',
+    '350 g de fromage gruyère râpé'
+  ],
+  instructions: [
+    'Faire revenir, dans votre poêlon à fondue, le beurre avec le piment, les poireaux et l’ail jusqu’à tendreté.',
+    'Ajouter ensuite la bière et la tomate. Laisser mijoter 2-3 minutes.',
+    'Incorporer avec une cuillère en bois le fromage, en petites quantités à la fois, ainsi que les épices italiennes.',
+    'Quand le fromage est bien fondu, laisser mijoter à feu doux quelques minutes.',
+    'Diluer ensuite la fécule de maïs dans un peu d’eau et verser la préparation, en petites quantités à la fois, jusqu’à l’obtention d’une belle texture qui collera bien sur le pain.',
+    'Déguster avec du pain frais.'
+  ],
+  tags: ['fromage', 'fondue', 'italien'],
+  slug: 'fondu-au-fromage-pas-ordinaire'
+};
