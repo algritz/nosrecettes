3723b986e714bbfd8a2268cffc53b589d24c6b06
David Cloutier <sheldreyn@gmail.com>
2025-12-23 11:13:10 -0500

    Add recipe: BigMac en tartare

---
+++ src/recipes/bigmac-en-tartare.ts
@@ -0,0 +1,34 @@
+import { Recipe } from '@/types/recipe';
+
+export const bigmacEnTartare: Recipe = {
+  id: 'bigmac-en-tartare',
+  title: 'BigMac en tartare',
+  description: 'Ça goûte vraiment le BigMac BigMac en tartare',
+  categories: ['Entrées'],
+  prepTime: 20,
+  cookTime: 0,
+  servings: 2,
+  difficulty: 'Facile',
+  ingredients: [
+    '200g de tartare de bœuf',
+    '2 c. à soupe d\'oignons hachés très très finement',
+    '3 c. à soupe de cornichons en brunoise',
+    '2 c. à soupe de cheddar orange en cube',
+    'Graines de sésame',
+    '1/4 tasse de mayonnaise',
+    '1 c. à soupe de vinaigrette française',
+    '1 c. à soupe de relish sucrée',
+    '1/2 c. à thé de sucre',
+    '1/2 c. à thé de vinaigre blanc',
+    '1 c. à thé d\'oignon haché très finement',
+    'Sel'
+  ],
+  instructions: [
+    'Dans un petit bol, mélanger les ingrédients pour la sauce. Réserver.',
+    'Mélanger les ingrédients du tartare et ajouter 2 à 3 cuillères à soupe de sauce.',
+    'Mouler à l\'aide d\'un emporte-pièce et servir.',
+    'Garnir le tartare de quelques graines de sésame.'
+  ],
+  tags: ['sésame', 'sauce mayonnaise', 'tartare'],
+  slug: 'bigmac-en-tartare'
+};
