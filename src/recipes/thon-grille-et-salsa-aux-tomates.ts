3a7897e8d590bc4f29a1658c59f56a9bb72a914e
David Cloutier <sheldreyn@gmail.com>
2025-12-23 15:58:14 -0500

    Add recipe: Thon grillé et salsa aux tomates

---
+++ src/recipes/thon-grille-et-salsa-aux-tomates.ts
@@ -0,0 +1,28 @@
+import { Recipe } from '@/types/recipe';
+
+export const thonGrilleEtSalsaAuxTomates: Recipe = {
+  id: 'thon-grille-et-salsa-aux-tomates',
+  title: 'Thon grillé et salsa aux tomates',
+  description: 'Un plat simple de thon grillé accompagné d\'une salsa fraîche aux tomates, citron, câpres et persil.',
+  categories: ['Vollaille', 'Plats principaux'],
+  prepTime: 20,
+  cookTime: 5,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '4 pavés de thon rouge de 150 g (5 oz) chacun',
+    '20 ml (4 c. à thé) d’huile d’olive',
+    'Sel et poivre du moulin',
+    '[object Object]'
+  ],
+  instructions: [
+    'Mélanger tous les ingrédients de la salsa. Laisser reposer à température ambiante 30 minutes avant de la servir avec les pavés de thon grillés.',
+    'Préchauffer le barbecue à feu vif.',
+    'Laisser tempérer le thon 20 minutes avant de le griller.',
+    'Saler et poivrer les pavés et les déposer sur une grande feuille de papier d’aluminium préalablement badigeonnée d’huile d’olive.',
+    'Déposer la feuille directement sur la grill du barbecue et cuire environ 2 minutes de chaque côté.',
+    'Servir le thon grillé avec la salsa.'
+  ],
+  tags: ['barbecue', 'salsa', 'thon'],
+  slug: 'thon-grille-et-salsa-aux-tomates'
+};
