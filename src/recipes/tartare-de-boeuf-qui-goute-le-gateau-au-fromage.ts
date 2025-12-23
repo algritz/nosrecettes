479763f4c211af5a8ca7c54889452d2885bbe6a6
David Cloutier <sheldreyn@gmail.com>
2025-12-23 14:53:45 -0500

    Add recipe: Tartare de boeuf qui goûte le gâteau au fromage

---
+++ src/recipes/tartare-de-boeuf-qui-goute-le-gateau-au-fromage.ts
@@ -0,0 +1,28 @@
+import { Recipe } from '@/types/recipe';
+
+export const tartareDeBoeufQuiGouteLeGateauAuFromage: Recipe = {
+  id: 'tartare-de-boeuf-qui-goute-le-gateau-au-fromage',
+  title: 'Tartare de boeuf qui goûte le gâteau au fromage',
+  description: 'Tartare de boeuf qui goûte le gâteau au fromage',
+  categories: ['Entrées'],
+  prepTime: 15,
+  cookTime: 0,
+  servings: 2,
+  difficulty: 'Facile',
+  ingredients: [
+    'boeuf à tartare coupé en petits cubes',
+    'Un filet d\'huile d\'olive',
+    '5 ml de sauce tamari',
+    '1 Oignon vert, ciselé',
+    '45 ml (3 c. à soupe) de fromage de chèvre aux canneberges, émietté',
+    '1/2 tasse de pacanes, grossièrement hachées',
+    '6 tiges de ciboulette, hachées'
+  ],
+  instructions: [
+    'Tout mélanger délicatement les ingrédients ensemble.'
+  ],
+  tags: ['fromage', 'tartare', 'olive'],
+  wine: 'https://www.saq.com/fr/14204442',
+  accompaniment: 'Craquelins aux fruits séchés',
+  slug: 'tartare-de-boeuf-qui-goute-le-gateau-au-fromage'
+};
