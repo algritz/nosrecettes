c34a8dda3fe98532680d45acf45ae23fa7701fb2
David Cloutier <sheldreyn@gmail.com>
2025-12-23 11:11:32 -0500

    Add recipe: Sauce aux abricots et aux prunes

---
+++ src/recipes/sauce-aux-abricots-et-aux-prunes.ts
@@ -0,0 +1,29 @@
+import { Recipe } from '@/types/recipe';
+
+export const sauceAuxAbricotsEtAuxPrunes: Recipe = {
+  id: 'sauce-aux-abricots-et-aux-prunes',
+  title: 'Sauce aux abricots et aux prunes',
+  description: '',
+  categories: ['Sauces'],
+  prepTime: 10,
+  cookTime: 20,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '100 ml d\'eau',
+    '2 prunes, en quartiers et sans noyaux',
+    '1 abricot, haché',
+    '30 ml de sucre',
+    '30 ml de miel liquide',
+    '1 bâton de cannelle',
+    '1 anis étoilé',
+    'Le jus d\'une demi-lime'
+  ],
+  instructions: [
+    'Mettre l’eau dans une petite casserole et porter à ébullition.',
+    'Ajouter les prunes, l’abricot, le sucre, le miel, la cannelle et l’anis étoilé, et cuire jusqu’à ce que le mélange ait réduit en une sauce collante.',
+    'Enlever du feu et mélanger en ajoutant le jus de lime.'
+  ],
+  tags: ['sauce', 'fruits', 'réduction'],
+  slug: 'sauce-aux-abricots-et-aux-prunes'
+};
