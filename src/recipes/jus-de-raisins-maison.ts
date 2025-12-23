efe3fbc2558be4c7e137835bcdbc194a0a5b81da
David Cloutier <sheldreyn@gmail.com>
2025-12-23 13:01:33 -0500

    Add recipe: Jus de raisins maison

---
+++ src/recipes/jus-de-raisins-maison.ts
@@ -0,0 +1,29 @@
+import { Recipe } from '@/types/recipe';
+
+export const jusDeRaisinsMaison: Recipe = {
+  id: 'jus-de-raisins-maison',
+  title: 'Jus de raisins maison',
+  description: 'Tellement une belle façon de passer des raisins frais',
+  categories: ['Végétarien'],
+  prepTime: 5,
+  cookTime: 10,
+  servings: 0,
+  difficulty: 'Facile',
+  ingredients: [
+    'Beaucoup de raisins frais'
+  ],
+  instructions: [
+    'Séparez les raisins de leurs tiges.',
+    'Lavez les raisins.',
+    'Mettez dans une passoire et lavez-les dans de l\'eau tiède afin d’éliminer tous les produits chimiques qui s’y trouveraient.',
+    'Écrasez les raisins délicatement avec un pilon ou une cuillère. Attention à ne pas faire de la purée de raisin.',
+    'Mettez les raisins en purée dans une casserole et faites cuire à feu moyen pendant environ 10 minutes.',
+    'Écrasez les raisins à l\'aide d\'une cuillère ou d’un pilon s’ils commencent à s\'agglutiner ou si des grains ne sont pas encore éclatés.',
+    'Filtrez le jus en mettant une passoire fine au-dessus d\'un récipient ou directement sur un verre à boire.',
+    'Versez les raisins dans la passoire, puis filtrez votre jus de raisin.',
+    'Retirez la passoire, puis mettez votre jus dans le réfrigérateur pour le refroidir ou mettez directement des glaçons dans votre verre.'
+  ],
+  tags: ['jus de raisin', 'fait maison', 'rafraîchissant'],
+  accompaniment: 'Absolument débile avec un peu de Vodka et une glace',
+  slug: 'jus-de-raisins-maison'
+};
