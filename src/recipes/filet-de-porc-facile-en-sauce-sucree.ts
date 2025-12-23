04d7fed31ced7a3511eb6bc538d2a3ca410d2642
David Cloutier <sheldreyn@gmail.com>
2025-12-23 11:01:46 -0500

    Add recipe: Filet de porc facile en sauce sucrée

---
+++ src/recipes/filet-de-porc-facile-en-sauce-sucree.ts
@@ -0,0 +1,25 @@
+import { Recipe } from '@/types/recipe';
+
+export const filetDePorcFacileEnSauceSucree: Recipe = {
+  id: 'filet-de-porc-facile-en-sauce-sucree',
+  title: 'Filet de porc facile en sauce sucrée',
+  description: '',
+  categories: ['Porc', 'Plats principaux'],
+  prepTime: 10,
+  cookTime: 45,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '2 filets de porc',
+    '3/4 tasse de sauce chili',
+    '3/4 tasse de sirop d\'érable'
+  ],
+  instructions: [
+    'Tout mélanger et mariner 8 à 12 heures minimum',
+    'Faire cuire au four à 350 F environ 35-45 minutes'
+  ],
+  tags: ['sauce sucrée', 'marinade', 'four'],
+  marinatingTime: 480,
+  notes: 'Excellent dépanneur la semaine',
+  slug: 'filet-de-porc-facile-en-sauce-sucree'
+};
