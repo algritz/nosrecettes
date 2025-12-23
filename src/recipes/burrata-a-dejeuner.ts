0f93fface2502704ff625b322cdc087e5d22e05d
David Cloutier <sheldreyn@gmail.com>
2025-12-23 11:20:27 -0500

    Add recipe: Burrata à déjeuner

---
+++ src/recipes/burrata-a-dejeuner.ts
@@ -0,0 +1,38 @@
+import { Recipe } from '@/types/recipe';
+
+export const burrataADejeuner: Recipe = {
+  id: 'burrata-a-dejeuner',
+  title: 'Burrata à déjeuner',
+  description: 'J\'adore cette recette, mon déjeuner préféré Burrata à déjeuner',
+  categories: ['Déjeuners', 'Végétarien'],
+  prepTime: 25,
+  cookTime: 20,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '½ tasse de sauce Hollandaise',
+    '3 saucisses à déjeuner coupé en rondelles',
+    '4 tranches de bacon haché grossièrement',
+    'Jambon haché grossièrement',
+    '1 petit oignon haché finement',
+    '1 c. à soupe de beurre',
+    '¼ de tasse de sirop d’érable',
+    '1 oz de Whisky',
+    'Une Burrata',
+    '1 c à soupe de Firebarns bacon ou sambal oelek',
+    'Sel et poivre au goût',
+    'Pousses fraîches pour garnir',
+    'Pain baguette pour servir'
+  ],
+  instructions: [
+    'Précuire le bacon et la saucisse, éponger sur un essuie-tout et trancher en petits morceaux le bacon et en rondelles les saucisses.',
+    'Dans une poêle à feu moyen, faire revenir l’oignon dans le beurre pendant 2 à 3 minutes.',
+    'Ajouter ensuite les saucisses, le bacon et le jambon.',
+    'Laisser cuire quelques minutes et déglacer avec le Whisky, ajouter ensuite le sirop d’érable, la Firebarns Bacon et continuer la cuisson à feu moyen pendant environ 5 à 10 minutes en remuant de temps en temps.',
+    'Placer la boule de Burrata au centre d’une assiette et déposer le mélange autour.',
+    'Verser ensuite quelques filets de sauces hollandaise sur le dessus de la Burrata.',
+    'Saler, poivrer et servir avec des pousses fraîches et du pain.'
+  ],
+  tags: ['burrata', 'saucisses', 'hollandaise'],
+  slug: 'burrata-a-dejeuner'
+};
