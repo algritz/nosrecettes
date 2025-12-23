a5cf2383260cddbc4dcb6873038f33d2f5021b00
David Cloutier <sheldreyn@gmail.com>
2025-12-23 15:21:18 -0500

    Add recipe: Salade rafraîchissante à l'orange

---
+++ src/recipes/salade-rafraichissante-a-l-orange.ts
@@ -0,0 +1,30 @@
+import { Recipe } from '@/types/recipe';
+
+export const saladeRafraichissanteALOrange: Recipe = {
+  id: 'salade-rafraichissante-a-l-orange',
+  title: 'Salade rafraîchissante à l\'orange',
+  description: 'Une salade fraîche avec des segments d\'orange, des légumes croquants et une vinaigrette légère à l\'huile d\'olive et miel.',
+  categories: ['Salades'],
+  prepTime: 15,
+  cookTime: 0,
+  servings: 0,
+  difficulty: 'Facile',
+  ingredients: [
+    'Salade 1 orange en petits morceaux',
+    '2 oignons verts en rondelles',
+    '3 oz (100 g) bébé épinards',
+    '6 radis tranchés fins',
+    '½ poivron jaune en cubes',
+    '¼ tasse (60 ml) huile d\'olive extra vierge',
+    '½ citron frais pressé',
+    '1 c. à soupe (15 ml) de miel',
+    '1 pincée poivre de Cayenne'
+  ],
+  instructions: [
+    'Dans un saladier, mélanger l\'orange, les oignons, les épinards, les radis et le poivron.',
+    'Dans un bol, mélanger les ingrédients de la vinaigrette.',
+    'Avant de servir, arroser la salade de la vinaigrette et brasser.'
+  ],
+  tags: ['fraîcheur', 'vinaigrette', 'citrus'],
+  slug: 'salade-rafraichissante-a-l-orange'
+};
