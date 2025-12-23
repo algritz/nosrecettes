beb8399ef00a392950472d51de46799a0a99bd71
David Cloutier <sheldreyn@gmail.com>
2025-12-23 11:17:39 -0500

    Add recipe: Tartare de bœuf fraises et vodka

---
+++ src/recipes/tartare-de-b-uf-fraises-et-vodka.ts
@@ -0,0 +1,38 @@
+import { Recipe } from '@/types/recipe';
+
+export const tartareDeBUfFraisesEtVodka: Recipe = {
+  id: 'tartare-de-b-uf-fraises-et-vodka',
+  title: 'Tartare de bœuf fraises et vodka',
+  description: '',
+  categories: ['Entrées'],
+  prepTime: 30,
+  cookTime: 15,
+  servings: 2,
+  difficulty: 'Facile',
+  ingredients: [
+    '400 g de bavette bœuf',
+    '75 g d’amandes',
+    '1 1/2 c. à soupe de miel',
+    '1 branche de romarin haché finement',
+    '1 c. à thé d’ail haché finement',
+    '100 g de câpres',
+    '5 tranches de prosciutto',
+    '300 g de fraises',
+    '1 c. à thé de sauce Worcestershire',
+    '1 c. à soupe de moutarde à l’ancienne',
+    '2 c. à thé d’huile d’olive',
+    '2 c. à thé de vinaigre balsamique',
+    '3/4 oz de vodka',
+    '1 c. à soupe d’épices à Steak Sauce piquante',
+    'Sel et poivre'
+  ],
+  instructions: [
+    'À feu moyen, mettre un peu d’huile dans un poêle et faire revenir les amandes avec le romarin, l’ail et finir avec le miel. Laisser refroidir un peu et hacher le tout finement.',
+    'Dans un four à 350 F, mettre les tranches de prosciutto sur une plaque pour en faire des chips, environ 10 minutes. Laisser refroidir et hacher grossièrement.',
+    'Faire frire les câpres.',
+    'Couper le bœuf et les fraises en petits cubes.',
+    'Mélanger le reste des ingrédients en vous assurant que les amandes, les câpres et le prosciutto sont refroidis avant de les mettre dans le tartare.'
+  ],
+  tags: ['bœuf', 'fraise', 'vodka'],
+  slug: 'tartare-de-b-uf-fraises-et-vodka'
+};
