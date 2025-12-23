3914c2ddef881632b88c554830e16c7430f82b92
David Cloutier <sheldreyn@gmail.com>
2025-12-23 12:18:01 -0500

    Add recipe: Tartare de thon coco avec salade de mangue et papaye

---
+++ src/recipes/tartare-de-thon-coco-avec-salade-de-mangue-et-papaye.ts
@@ -0,0 +1,22 @@
+import { Recipe } from '@/types/recipe';
+
+export const tartareDeThonCocoAvecSaladeDeMangueEtPapaye: Recipe = {
+  id: 'tartare-de-thon-coco-avec-salade-de-mangue-et-papaye',
+  title: 'Tartare de thon coco avec salade de mangue et papaye',
+  description: 'Un tartare de thon rouge accompagné d\'une salade fraîche de mangue et papaye, relevé de lait de coco, coriandre et piments chili.',
+  categories: ['Entrées'],
+  prepTime: 30,
+  cookTime: 0,
+  servings: 2,
+  difficulty: 'Facile',
+  ingredients: [
+
+  ],
+  instructions: [
+    'Salade: Dans un bol, mélanger tous les ingrédients pour la salade de papaye et de mangue.',
+    'Tartare: Dans un autre bol, mélanger le thon, l\'ail et le jus de lime. Assaisonner de sel et brasser délicatement. Ajouter le reste des ingrédients.',
+    'Servir le tartare, accompagné de salade de mangue et de papaye.'
+  ],
+  tags: ['fraîcheur', 'coco', 'mangue'],
+  slug: 'tartare-de-thon-coco-avec-salade-de-mangue-et-papaye'
+};
