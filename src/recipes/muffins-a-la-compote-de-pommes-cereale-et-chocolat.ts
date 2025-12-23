561a8447f1d91bedf3c76cccc8ca112e7f41b1e7
David Cloutier <sheldreyn@gmail.com>
2025-12-23 12:47:36 -0500

    Add recipe: Muffins à la compote de pommes, céréale et chocolat

---
+++ src/recipes/muffins-a-la-compote-de-pommes-cereale-et-chocolat.ts
@@ -0,0 +1,37 @@
+import { Recipe } from '@/types/recipe';
+
+export const muffinsALaCompoteDePommesCerealeEtChocolat: Recipe = {
+  id: 'muffins-a-la-compote-de-pommes-cereale-et-chocolat',
+  title: 'Muffins à la compote de pommes, céréale et chocolat',
+  description: '',
+  categories: ['Pâtisseries et desserts'],
+  prepTime: 15,
+  cookTime: 22,
+  servings: 12,
+  difficulty: 'Facile',
+  ingredients: [
+    '2 tasses de céréales Cheerios',
+    '1 1/4 tasses de farine tout usage',
+    '1/3 tasse de cassonade, légèrement tassée',
+    '1 c. à thé de poudre à pâte',
+    '1 c. à thé de cannelle moulue',
+    '3/4 c. à thé de bicarbonate de sodium',
+    '1 tasse de compote de pommes',
+    '1/3 tasse de lait',
+    '3 c. à soupe d\'huile végétale',
+    '1 œuf',
+    '1/2 tasse de mini chips de chocolat'
+  ],
+  instructions: [
+    'Préchauffer le four à 400 °F',
+    'Préparer 12 moules à muffins',
+    'Mettre les céréales dans un sac à aliments en plastique et le fermer. À l\'aide d\'un rouleau à pâtisserie, broyer les céréales; verser dans un grand bol.',
+    'Ajouter la farine, la cassonade, la poudre à pâte, la cannelle et le bicarbonate de sodium et bien mélanger.',
+    'Incorporer le reste des ingrédients, jusqu\'à ce qu\'ils soient tous humectés.',
+    'Répartir la préparation uniformément dans les moules.',
+    'Faire cuire de 20 à 22 minutes ou jusqu\'à obtention d\'une coloration dorée.',
+    'Laisser refroidir 5 minutes avant de servir.'
+  ],
+  tags: ['céréales', 'chocolat', 'muffins'],
+  slug: 'muffins-a-la-compote-de-pommes-cereale-et-chocolat'
+};
