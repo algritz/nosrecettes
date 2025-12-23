2c6d4a3dc36c3a77a3fb359a48c4ce32f05c5623
David Cloutier <sheldreyn@gmail.com>
2025-12-23 13:53:59 -0500

    Add recipe: Tartare de bœuf au chorizo et salade de roquette mayonnaise au paprika fumé

---
+++ src/recipes/tartare-de-b-uf-au-chorizo-et-salade-de-roquette-mayonnaise-au-paprika-fume.ts
@@ -0,0 +1,42 @@
+import { Recipe } from '@/types/recipe';
+
+export const tartareDeBUfAuChorizoEtSaladeDeRoquetteMayonnaiseAuPaprikaFume: Recipe = {
+  id: 'tartare-de-b-uf-au-chorizo-et-salade-de-roquette-mayonnaise-au-paprika-fume',
+  title: 'Tartare de bœuf au chorizo et salade de roquette mayonnaise au paprika fumé',
+  description: 'Tartare de bœuf au chorizo et salade de roquette mayonnaise au paprika fumé',
+  categories: ['Entrées'],
+  prepTime: 25,
+  cookTime: 0,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '480 g Boeuf Boston',
+    '1 Échalote',
+    '5 brins de ciboulette',
+    '5 brins de Persil',
+    '1 saucisse Chorizo espagnol',
+    '15 ml câpres',
+    '1 jaune d\'oeuf',
+    '15 ml jus de citron',
+    '15 ml moutarde de Dijon',
+    '2 ml paprika fumé',
+    '200 ml huile végétale',
+    '4 poignées de roquette'
+  ],
+  instructions: [
+    'Coupez le bœuf au couteau en petits dés et réservez-les au frais dans un bol.',
+    'Ciselez finement l\'échalote, le persil et la ciboulette.',
+    'Coupez le chorizo en petits cubes.',
+    'Hachez les câpres finement.',
+    'Dans un bol, mélangez un jaune d\'oeuf avec la moutarde de Dijon et le jus de citron.',
+    'À l\'aide d\'un fouet, incorporez l\'huile végétale en filet tout en fouettant constamment afin d’émulsionner le mélange jusqu\'à l\'obtention d\'une mayonnaise ferme.',
+    'Salez et poivrez et ajoutez le paprika fumé.',
+    'Dans un bol, réunissez l\'échalote, la ciboulette, le persil, les câpres, les dés de bœuf et le chorizo.',
+    'Mélangez bien.',
+    'Liez avec un peu de mayonnaise au paprika fumé, salez et poivrez selon votre goût et mélangez.',
+    'Servir la tartare à l\'aide d\'un emporte pièce.',
+    'Garnir de roquette et mayonnaise au paprika fumé.'
+  ],
+  tags: ['bœuf', 'chorizo', 'mayonnaise'],
+  slug: 'tartare-de-b-uf-au-chorizo-et-salade-de-roquette-mayonnaise-au-paprika-fume'
+};
