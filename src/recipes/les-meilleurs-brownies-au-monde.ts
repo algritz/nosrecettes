d43a2dc5b8263dd736a816929738e8e2e4f0072c
David Cloutier <sheldreyn@gmail.com>
2025-12-23 13:33:28 -0500

    Add recipe: Les meilleurs Brownies au monde

---
+++ src/recipes/les-meilleurs-brownies-au-monde.ts
@@ -0,0 +1,33 @@
+import { Recipe } from '@/types/recipe';
+
+export const lesMeilleursBrowniesAuMonde: Recipe = {
+  id: 'les-meilleurs-brownies-au-monde',
+  title: 'Les meilleurs Brownies au monde',
+  description: '',
+  categories: ['Pâtisseries et desserts'],
+  prepTime: 20,
+  cookTime: 30,
+  servings: 0,
+  difficulty: 'Facile',
+  ingredients: [
+    '2 tasses (500 ml) de chocolat noir 70 % de cacao en pastilles',
+    '2/3 tasse (150 ml) de beurre non salé',
+    '3 oeufs',
+    '1 tasse (250 ml) de sucre',
+    '2 c. à soupe (30 ml) de sirop de maïs',
+    '1/3 tasse (75 ml) de crème 35 %',
+    '2/3 tasse (150 ml) de farine tout usage'
+  ],
+  instructions: [
+    'Préchauffer le four à 350 °F (180 °C).',
+    'Au bain-marie, faire fondre le chocolat et le beurre; réserver.',
+    'Dans un bol, à l\'aide d\'un fouet, blanchir les oeufs avec le sucre et le sirop de maïs.',
+    'Incorporer le mélange de chocolat, puis la crème.',
+    'Ajouter la farine et mélanger jusqu\'à l\'obtention d\'une pâte homogène.',
+    'Beurrer et tapisser le fond d\'un moule carré de 8 po (20 cm) et cuire au centre du four environ 30 minutes; le centre du gâteau doit être encore tremblotant.',
+    'Laisser reposer à température pièce.',
+    'Démouler et couper les brownies en cubes.'
+  ],
+  tags: ['chocolat', 'gâteau fondant', 'croustillant'],
+  slug: 'les-meilleurs-brownies-au-monde'
+};
