33e221e896ab63501bd316dbc65755761decaa6f
David Cloutier <sheldreyn@gmail.com>
2025-12-23 17:20:48 -0500

    Add recipe: Tartare de bœuf, framboises et basilic

---
+++ src/recipes/tartare-de-b-uf-framboises-et-basilic.ts
@@ -0,0 +1,29 @@
+import { Recipe } from '@/types/recipe';
+
+export const tartareDeBUfFramboisesEtBasilic: Recipe = {
+  id: 'tartare-de-b-uf-framboises-et-basilic',
+  title: 'Tartare de bœuf, framboises et basilic',
+  description: 'Tartare de bœuf, framboises et basilic',
+  categories: ['Entrées'],
+  prepTime: 15,
+  cookTime: 0,
+  servings: 2,
+  difficulty: 'Facile',
+  ingredients: [
+    '250 gr de bœuf coupé en petits dés',
+    '2 cuillères à soupe d’huile d’olive',
+    '1 cuillère à soupe de vinaigre de framboises (ou vinaigre de vin rouge)',
+    '½ cuillère à thé de moutarde de Dijon',
+    '10 câpres écrasées',
+    '20 framboises lavées coupée en deux',
+    '2 - 3 grosses feuilles de basilic grossièrement émincées',
+    'Sel et poivre',
+    '1 cuillère à soupe ou moins de sauce épicée de votre choix'
+  ],
+  instructions: [
+    'Mettre tous les ingrédients dans un bol, mélangez.',
+    'Ajoutez les framboises et mélangez tranquillement pour ne pas les écraser.'
+  ],
+  tags: ['bœuf cru', 'framboises', 'basilic'],
+  slug: 'tartare-de-b-uf-framboises-et-basilic'
+};
