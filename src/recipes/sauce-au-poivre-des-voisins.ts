5dd21f528890c33bd98e0f9a44481642abf49135
David Cloutier <sheldreyn@gmail.com>
2025-12-24 13:07:47 -0500

    Add recipe: Sauce au poivre des voisins (#1209)

---
+++ src/recipes/sauce-au-poivre-des-voisins.ts
@@ -0,0 +1,27 @@
+import { Recipe } from '@/types/recipe';
+
+export const sauceAuPoivreDesVoisins: Recipe = {
+  id: 'sauce-au-poivre-des-voisins',
+  title: 'Sauce au poivre des voisins',
+  description: 'Sauce au poivre de nos voisin de chalet, elle est excellente.',
+  categories: ['Sauces'],
+  prepTime: 10,
+  cookTime: 0,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '1 c. à thé de Bovril au bœuf',
+    '1 c. à thé de Bovril au poulet',
+    '2 c. à thé de farine',
+    '1/8 c. à thé de moutarde sèche',
+    '1/4 tasse d’eau tiède',
+    '1/4 tasse de Southern Comfort ou autre boisson',
+    '1/2 tasse de crème 35%'
+  ],
+  instructions: [
+    'Mélanger les ingrédients secs dans un chaudron et ajouter tous les liquides.',
+    'Cuire sur feu doux en brassant continuellement jusqu’à épaississement de la sauce.'
+  ],
+  tags: ['sauce', 'poivre', 'réconfort'],
+  slug: 'sauce-au-poivre-des-voisins'
+};
