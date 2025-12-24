9c70bcfe3e9bf3ced2b80878f25109c2c0471949
David Cloutier <sheldreyn@gmail.com>
2025-12-24 13:23:16 -0500

    Add recipe: Navarin de canard à l’érable (#1197)

---
+++ src/recipes/navarin-de-canard-a-l-erable.ts
@@ -0,0 +1,39 @@
+import { Recipe } from '@/types/recipe';
+
+export const navarinDeCanardALErable: Recipe = {
+  id: 'navarin-de-canard-a-l-erable',
+  title: 'Navarin de canard à l’érable',
+  description: 'Un plat savoureux de canard mijoté avec des légumes racines et une sauce à l’érable, parfait pour une cuisine réconfortante.',
+  categories: ['Plats principaux'],
+  prepTime: 20,
+  cookTime: 45,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '12 morceaux de rutabaga (chou-navet à chair jaune), pelés et taillés en boule',
+    '12 morceaux de carotte, pelés et taillés en boules',
+    '2 beaux magrets de canard frais d\'au moins 400 g (3/4 lb) chacun',
+    '2 échalotes françaises, hachées',
+    '2 c. à table (30 ml) vin blanc',
+    '2 c. à table (30 ml) vinaigre d\'érable',
+    '2 c. à table (30 ml) sirop d\'érable (ambré de préférence)',
+    '1 tasse (250 ml) sauce demi-glace (sauce brune maison ou du commerce), chaude',
+    '1/2 tasse (125 ml) pois verts, blanchis',
+    'Sel et poivre noir frais moulu'
+  ],
+  instructions: [
+    'Dans une grande casserole d\'eau bouillante salée, blanchir les légumes en boule environ 5 minutes. Réserver au frais.',
+    'Retirer la partie grasse sur le dessus des magrets de canard et couper leur chair en dés.',
+    'Dans une sauteuse, faire fondre le gras des magrets à feu doux. Retirer tout le gras fondu, sauf 30 ml (2 c. à tab), et réserver.',
+    'À feu vif et lorsque le gras de la sauteuse atteint son point de fumée, ajouter la moitié des dés de canard, assaisonner et saisir. Vous voulez colorer l\'extérieur tout en gardant l\'intérieur le plus saignant possible. Réserver.',
+    'Répéter avec le reste des dés de canard et réserver aussi.',
+    'Dans le gras de cuisson, faire sauter les échalotes. Ajouter un peu de gras de canard réservé au besoin.',
+    'Déglacer avec le vin blanc, laisser réduire légèrement et réserver ce jus de cuisson.',
+    'Dans une casserole, combiner le vinaigre et le sirop d\'érable, puis cuire jusqu\'à l\'obtention d\'un caramel coloré.',
+    'Mouiller avec le jus de cuisson réservé et la demi-glace.',
+    'Ajouter les carottes et le rutabaga blanchis, puis laisser mijoter jusqu\'à ce que les légumes soient tendres.',
+    'Ajouter le canard sauté et les pois verts. Laisser mijoter 2 minutes pour lier les saveurs et servir.'
+  ],
+  tags: ['érable', 'sauté', 'mijoter'],
+  slug: 'navarin-de-canard-a-l-erable'
+};
