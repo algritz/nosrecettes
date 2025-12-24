6ff42f33e7139b1142b73190fb3d8d4d4e602d73
David Cloutier <sheldreyn@gmail.com>
2025-12-24 13:33:55 -0500

    Add recipe: Soupe chargée aux brocolis pommes de terre au cheddar (#1186)

---
+++ src/recipes/soupe-chargee-aux-brocolis-pommes-de-terre-au-cheddar.ts
@@ -0,0 +1,49 @@
+import { Recipe } from '@/types/recipe';
+
+export const soupeChargeeAuxBrocolisPommesDeTerreAuCheddar: Recipe = {
+  id: 'soupe-chargee-aux-brocolis-pommes-de-terre-au-cheddar',
+  title: 'Soupe chargée aux brocolis pommes de terre au cheddar',
+  description: 'Une soupe réconfortante et crémeuse aux brocolis, pommes de terre, cheddar et bacon, parfaite pour un repas chaud.',
+  categories: ['Soupes'],
+  prepTime: 25,
+  cookTime: 25,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '1 cuillère à soupe de beurre',
+    '4 cuillères à soupe de beurre',
+    '1 petit oignon coupé en petits dés',
+    '4 carottes de taille moyenne coupées en petits dés',
+    '4 gousses d\'ail hachées',
+    '4 pommes de terre moyennes coupées en petits dés',
+    '1 litre de bouillon de poulet',
+    '2 petites têtes de brocoli coupées en petits dés',
+    '⅓ tasse de farine',
+    '3 ½ tasses de lait entier',
+    '1 ½ cuillères à café de sel casher',
+    '½ cuillère à café de poivre noir',
+    '4 tasses de fromage cheddar',
+    '¾ cuillère à café de sauce Tabasco',
+    '3 tranches de bacon cuit et haché grossièrement'
+  ],
+  instructions: [
+    'Dans une grande casserole à feu moyen-vif, faire fondre 1 cuillère à soupe de beurre.',
+    'Ajouter les oignons et faire sauter jusqu\'à ce qu\'ils soient translucides.',
+    'Ajouter les carottes et continuer à faire cuire de 3 à 4 minutes.',
+    'Ajouter l\'ail et cuire jusqu\'à ce qu\'il soit parfumé.',
+    'Ajouter les pommes de terre et le bouillon de poulet, puis porter à ébullition.',
+    'Réduire le feu et laisser mijoter pendant environ 10 minutes.',
+    'Ajouter les brocolis et laisser mijoter pendant 10 minutes, ou jusqu\'à ce qu\'ils soient tendres.',
+    'Pendant que la soupe mijote, faire fondre le reste de beurre dans un grand chaudron.',
+    'Ajouter en fouettant la farine et cuire jusqu\'à coloration dorée, environ 1 minute.',
+    'Ajouter le lait et continuer à remuer jusqu\'à ce que la sauce épaississe.',
+    'Ajouter le fromage et remuer jusqu\'à ce qu\'il soit fondu.',
+    'Assaisonner avec le sel, le poivre et la sauce Tabasco.',
+    'Verser la sauce au fromage dans le chaudron avec les légumes et le bouillon.',
+    'Remuer jusqu\'à ce que le tout soit bien mélangé.',
+    'Goûter et ajuster l\'assaisonnement si nécessaire.',
+    'Garnir avec le bacon haché et servir chaud.'
+  ],
+  tags: ['fromage', 'bacon', 'soupe'],
+  slug: 'soupe-chargee-aux-brocolis-pommes-de-terre-au-cheddar'
+};
