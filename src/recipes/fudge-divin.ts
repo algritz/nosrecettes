e31fee935b8410b240c11b6046f5ff0c9da1a12d
David Cloutier <sheldreyn@gmail.com>
2025-12-24 14:48:51 -0500

    Add recipe: Fudge divin (#1107)

---
+++ src/recipes/fudge-divin.ts
@@ -0,0 +1,31 @@
+import { Recipe } from '@/types/recipe';
+
+export const fudgeDivin: Recipe = {
+  id: 'fudge-divin',
+  title: 'Fudge divin',
+  description: '',
+  categories: ['Pâtisseries et desserts'],
+  prepTime: 10,
+  cookTime: 30,
+  servings: 35,
+  difficulty: 'Facile',
+  ingredients: [
+    '4 tasses de sucre',
+    '1 ½ tasse de lait',
+    '¾ tasse de beurre',
+    '1 paquet de capuchons de chocolat mi-sucré (300 g)',
+    '2 tasses de guimauves (La grosseur n\'a pas d\'importance)',
+    '1 ½ c. à thé de vanille'
+  ],
+  instructions: [
+    'Beurrer un moule de 12 x 7 x 1 ½ pouce.',
+    'Mélanger le sucre, le lait et le beurre.',
+    'Cuire à feu moyen en brassant souvent, jusqu’à 234° au thermomètre à bonbon ou jusqu’à ce que quelques gouttes du mélange forment une boule dans un verre d’eau froide.',
+    'Retirer du feu.',
+    'Ajouter les brisures de chocolat, les guimauves et la vanille.',
+    'Battre vigoureusement jusqu’à ce que tout soit complètement fondu.',
+    'Verser dans le moule et réfrigérer jusqu’à ferme.'
+  ],
+  tags: ['chocolat', 'guimauve', 'fudge'],
+  slug: 'fudge-divin'
+};
