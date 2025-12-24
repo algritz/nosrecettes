f65b986a975c96b9bdecf3567182e6b425e45507
David Cloutier <sheldreyn@gmail.com>
2025-12-24 12:15:08 -0500

    Add recipe: Légumes rôtis dans une sauce au vinaigre balsamique et à l'érable (#1262)

---
+++ src/recipes/legumes-rotis-dans-une-sauce-au-vinaigre-balsamique-et-a-l-erable.ts
@@ -0,0 +1,34 @@
+import { Recipe } from '@/types/recipe';
+
+export const legumesRotisDansUneSauceAuVinaigreBalsamiqueEtALErable: Recipe = {
+  id: 'legumes-rotis-dans-une-sauce-au-vinaigre-balsamique-et-a-l-erable',
+  title: 'Légumes rôtis dans une sauce au vinaigre balsamique et à l\'érable',
+  description: '',
+  categories: ['Légumes', 'Plats principaux'],
+  prepTime: 15,
+  cookTime: 50,
+  servings: 3,
+  difficulty: 'Facile',
+  ingredients: [
+    '1 tasse de choux de Bruxelles, bases coupées et première couche de feuilles enlevée',
+    '6 champignons blancs, coupés en 4',
+    '3 ou 4 petites pommes de terre nouvelles, coupées en 4 avec la pelure',
+    '½ oignon rouge, tranché en grosses lamelles',
+    'Quelques poignées de graines de tournesol, non salées',
+    '1 c. à soupe de bouillon de légumes concentré',
+    '1 c. à soupe d\'huile d\'olive vierge ou extra-vierge',
+    '1 c. à thé de vinaigre balsamique',
+    '2 à 3 c. à soupe de sirop d\'érable',
+    '2 c. à soupe de sauce soya'
+  ],
+  instructions: [
+    'Blanchir les choux de Bruxelles et les pommes de terre. Pour ce faire, amener à ébullition une casserole d\'eau salée et y plonger les légumes. Cuire environ 8 à 10 minutes et retirer rapidement du feu. Verser dans une passoire et passer les légumes sous l\'eau froide afin de stopper la cuisson. Égoutter.',
+    'Préchauffer le four à 400 °F.',
+    'Déposer tous les légumes dans un plat à cuisson allant au four.',
+    'Mélanger tous les ingrédients de la sauce dans un petit bol : le bouillon concentré, l\'huile d\'olive, le vinaigre balsamique, le sirop d\'érable, la sauce soya et les graines de tournesol.',
+    'Verser la sauce sur les légumes et mélanger le tout.',
+    'Enfourner pendant 40 à 50 minutes.'
+  ],
+  tags: ['roast', 'balsamique', 'érable'],
+  slug: 'legumes-rotis-dans-une-sauce-au-vinaigre-balsamique-et-a-l-erable'
+};
