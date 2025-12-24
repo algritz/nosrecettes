939a43f433b92685a510b07817a545da0e3afc62
David Cloutier <sheldreyn@gmail.com>
2025-12-24 12:18:04 -0500

    Add recipe: Tartare de thon aux fruits coco-cari (#1258)

---
+++ src/recipes/tartare-de-thon-aux-fruits-coco-cari.ts
@@ -0,0 +1,34 @@
+import { Recipe } from '@/types/recipe';
+
+export const tartareDeThonAuxFruitsCocoCari: Recipe = {
+  id: 'tartare-de-thon-aux-fruits-coco-cari',
+  title: 'Tartare de thon aux fruits coco-cari',
+  description: 'Un tartare frais de thon, banane et pomme, relevé de citron vert, lait de coco et cari, servi après deux périodes de marinade au frais.',
+  categories: ['Entrées'],
+  prepTime: 20,
+  cookTime: 0,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '700g de thon coupé en petits dés',
+    '1/2 banane pas trop molle coupée en dés',
+    '1/2 pomme granny smith coupée en dés',
+    'Oignons verts ciselés ou ciboulette au goût',
+    'Sel & Poivre',
+    '5 cuillères à soupe de lait de coco',
+    'Le jus d\'un citron vert',
+    'Cari fort au goût'
+  ],
+  instructions: [
+    'Dans un récipient non métallique, déposez le poisson, la banane et la pomme.',
+    'Arroser de jus de citron vert, salez, poivrez, ajoutez les oignons verts ciselés mais gardez-en un peu pour la décoration.',
+    'Mélangez.',
+    'Disposez 15 minutes au frais.',
+    'Après les 15 minutes, incorporez le lait de coco et le cari à la préparation.',
+    'Rectifiez l\'assaisonnement.',
+    'Placez de nouveau 15 minutes au frais puis servez.'
+  ],
+  tags: ['coco', 'cari', 'frais'],
+  marinatingTime: 30,
+  slug: 'tartare-de-thon-aux-fruits-coco-cari'
+};
