783e987c1055512eab1862572b9e093df22565ca
David Cloutier <sheldreyn@gmail.com>
2025-12-24 14:18:49 -0500

    Add recipe: Sauce au yogourt, à la coriandre et à la lime (#1140)

---
+++ src/recipes/sauce-au-yogourt-a-la-coriandre-et-a-la-lime.ts
@@ -0,0 +1,26 @@
+import { Recipe } from '@/types/recipe';
+
+export const sauceAuYogourtALaCoriandreEtALaLime: Recipe = {
+  id: 'sauce-au-yogourt-a-la-coriandre-et-a-la-lime',
+  title: 'Sauce au yogourt, à la coriandre et à la lime',
+  description: '',
+  categories: ['Sauces'],
+  prepTime: 5,
+  cookTime: 0,
+  servings: 0,
+  difficulty: 'Facile',
+  ingredients: [
+    '65 ml de yogourt méditerranéen',
+    '5 ml de jus de lime',
+    '1 lime, pour le zeste',
+    '90 ml de coriandre fraîche, hachée finement',
+    '15 ml d\'huile d\'olive',
+    'Sel et poivre, au goût'
+  ],
+  instructions: [
+    'Dans un bol, mélanger tous les ingrédients.'
+  ],
+  tags: ['coriandre', 'lime', 'sauce'],
+  accompaniment: 'Servir avec les cuisses de poulet façon teriyaki',
+  slug: 'sauce-au-yogourt-a-la-coriandre-et-a-la-lime'
+};
