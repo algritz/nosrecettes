774196c976d34e57c38fa3cc69966ac8080b5eb5
David Cloutier <sheldreyn@gmail.com>
2025-12-24 13:50:21 -0500

    Add recipe: Côtelettes de porc BBQ à l'ancienne (#1169)

---
+++ src/recipes/cotelettes-de-porc-bbq-a-l-ancienne.ts
@@ -0,0 +1,33 @@
+import { Recipe } from '@/types/recipe';
+
+export const cotelettesDePorcBbqALAncienne: Recipe = {
+  id: 'cotelettes-de-porc-bbq-a-l-ancienne',
+  title: 'Côtelettes de porc BBQ à l\'ancienne',
+  description: '',
+  categories: ['Porc', 'Plats principaux'],
+  prepTime: 20,
+  cookTime: 75,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '4 à 6 côtelettes de porc',
+    '1 c. à table de beurre',
+    '1 boîte de crème de tomates',
+    '1/2 tasse d\'eau',
+    '1 oignon haché',
+    '1/2 tasse de céleri haché',
+    '1/2 tasse de carottes en rondelles',
+    '2 c. à table de sucre brun',
+    '1 c. à table de moutarde',
+    '1 c. à table de sauce Worcestershire',
+    '2 c. à table de vinaigre de vin blanc',
+    'Poivre de Cayenne au goût'
+  ],
+  instructions: [
+    'Faire revenir les côtelettes dans le beurre. Dorer des deux côtés.',
+    'Mélanger tous les autres ingrédients dans un plat allant au four et y ajouter les côtelettes de porc.',
+    'Cuire au four à 350 °F pendant environ 1h15.'
+  ],
+  tags: ['barbecue', 'porc', 'sauce'],
+  slug: 'cotelettes-de-porc-bbq-a-l-ancienne'
+};
