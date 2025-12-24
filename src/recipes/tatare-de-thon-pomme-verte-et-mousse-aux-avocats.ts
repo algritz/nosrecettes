c813cf3785cc56707c1682a506fe3584adcd2742
David Cloutier <sheldreyn@gmail.com>
2025-12-24 15:37:29 -0500

    Add recipe: Tatare de thon, pomme verte et mousse aux avocats (#1058)

---
+++ src/recipes/tatare-de-thon-pomme-verte-et-mousse-aux-avocats.ts
@@ -0,0 +1,40 @@
+import { Recipe } from '@/types/recipe';
+
+export const tatareDeThonPommeVerteEtMousseAuxAvocats: Recipe = {
+  id: 'tatare-de-thon-pomme-verte-et-mousse-aux-avocats',
+  title: 'Tatare de thon, pomme verte et mousse aux avocats',
+  description: 'Un tartare de thon frais accompagné d\'une mousse onctueuse à l\'avocat et de la fraîcheur de la pomme verte, idéal pour une entrée élégante.',
+  categories: ['Entrées'],
+  prepTime: 15,
+  cookTime: 0,
+  servings: 2,
+  difficulty: 'Facile',
+  ingredients: [
+    '500g de thon',
+    '1 pomme Granny Smith',
+    '14 brins de ciboulette',
+    '1 échalote française',
+    '1 citron vert',
+    '15g de gingembre frais',
+    '2 avocats',
+    '1 gousse d’ail',
+    '4 gouttes de Sriracha',
+    'huile d\'olive',
+    'Sel et poivre du moulin'
+  ],
+  instructions: [
+    'Zestez le citron vert et récupérez son jus.',
+    'Coupez la pomme en tout petits cubes et mélangez-la immédiatement avec quelque gouttes de jus de citron vert.',
+    'Rapez ou hachez le gingembre afin d’obtenir une fine purée.',
+    'Ciselez la ciboulette et l’échalote.',
+    'Récupérez la chair de l’avocat.',
+    'Dans un robot-mélangeur, mettez la chair d’avocat, le reste du jus de citron vert, la gousse d’ail hachée finement et la sriracha. Mixez jusqu’à l’obtention d’une mousse lisse. Rectifiez l’assaisonnement et réservez.',
+    'Dans un bol, mélangez le thon avec la pomme, les zestes de citron vert, le gingembre, l’échalote et la ciboulette.',
+    'Coupez le thon en très petits dés.',
+    'Ajoutez un filet d’huile d’olive, du sel et du poivre et réservez.',
+    'Déposez le tartare dans un emporte-pièce, disposez dessus un peu de mousse d’avocat.',
+    'Servez avec des croûtons.'
+  ],
+  tags: ['avocat', 'thon', 'entrée'],
+  slug: 'tatare-de-thon-pomme-verte-et-mousse-aux-avocats'
+};
