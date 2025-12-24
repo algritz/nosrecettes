2169e206a44624d27f615a6a7ea4dfa1748bc48c
David Cloutier <sheldreyn@gmail.com>
2025-12-23 22:29:23 -0500

    Add recipe: Porc au caramel, bok choy et vermicelles

---
+++ src/recipes/porc-au-caramel-bok-choy-et-vermicelles.ts
@@ -0,0 +1,37 @@
+import { Recipe } from '@/types/recipe';
+
+export const porcAuCaramelBokChoyEtVermicelles: Recipe = {
+  id: 'porc-au-caramel-bok-choy-et-vermicelles',
+  title: 'Porc au caramel, bok choy et vermicelles',
+  description: 'Un plat savoureux de porc au caramel accompagné de bok choy, carottes, oignons verts et vermicelles de riz, avec une sauce soja et gingembre.',
+  categories: ['Plats principaux'],
+  prepTime: 25,
+  cookTime: 15,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '600 g de filet de porc, coupé en tranches minces',
+    '1/4 de tasse de cassonade',
+    'Sel et poivre, au goût',
+    '1 c. à soupe d\'huile végétale',
+    '1 1/2 tasse de carottes, râpées',
+    '2 oignons verts, émincés',
+    '1 1/2 tasse de bok choy, émincé',
+    '1 1/2 c. à soupe de fécule de maïs',
+    '1/4 de tasse de sauce soja',
+    '1 1/2 tasse de bouillon de poulet',
+    '1 c. à soupe de gingembre frais, pelé et haché finement',
+    '1/4 c. à thé de mélange cinq épices'
+  ],
+  instructions: [
+    'Cuire les vermicelles de riz comme indiqué sur l’emballage.',
+    'Dans un bol, diluer la fécule de maïs dans la sauce soja, puis ajouter le bouillon de poulet, le gingembre et le mélange cinq épices. Réserver.',
+    'Dans un autre bol, mélanger le porc et la cassonade. Assaisonner.',
+    'Dans une grande poêle, chauffer l’huile à feu vif, puis saisir le porc pendant 2 à 3 minutes, ou jusqu’à l’obtention d’une belle coloration.',
+    'Ajouter les carottes, les oignons verts et le bok choy, puis bien remuer.',
+    'Verser le mélange liquide dans la poêle et cuire à feu moyen pendant 5 à 6 minutes, ou jusqu’à l’obtention d’une sauce épaisse.',
+    'Servir le porc sur les vermicelles de riz.'
+  ],
+  tags: ['caramel', 'sauce soja', 'bok choy'],
+  slug: 'porc-au-caramel-bok-choy-et-vermicelles'
+};
