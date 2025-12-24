b698e803eff639fd7c6471589b1267af796a0b48
David Cloutier <sheldreyn@gmail.com>
2025-12-23 22:14:46 -0500

    Add recipe: Carrés au caramel de la voisine de chalet

---
+++ src/recipes/carres-au-caramel-de-la-voisine-de-chalet.ts
@@ -0,0 +1,26 @@
+import { Recipe } from '@/types/recipe';
+
+export const carresAuCaramelDeLaVoisineDeChalet: Recipe = {
+  id: 'carres-au-caramel-de-la-voisine-de-chalet',
+  title: 'Carrés au caramel de la voisine de chalet',
+  description: 'Carrés au caramel de la voisine de chalet',
+  categories: ['Pâtisseries et desserts'],
+  prepTime: 10,
+  cookTime: 20,
+  servings: 25,
+  difficulty: 'Facile',
+  ingredients: [
+    '1 boîte biscuit Ritz original',
+    '1 boîte lait Eagle Brand',
+    '1 sac de chipits au caramel'
+  ],
+  instructions: [
+    'Graisser un moule de 8 x 8 pouces.',
+    'Émietter le sac de biscuits.',
+    'Mettre les biscuits dans un bol, puis ajouter la boîte de lait Eagle Brand et les 3/4 du sac de pépites.',
+    'Mettre le mélange dans le moule et saupoudrer du reste des pépites.',
+    'Faire cuire au four à 350 °F pendant 20 minutes'
+  ],
+  tags: ['caramel', 'pâtisserie', 'four'],
+  slug: 'carres-au-caramel-de-la-voisine-de-chalet'
+};
