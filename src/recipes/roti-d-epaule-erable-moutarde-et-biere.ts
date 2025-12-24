52c7814d7af09f0cbbbc15de75c4b1562054e35e
David Cloutier <sheldreyn@gmail.com>
2025-12-24 18:16:41 -0500

    Add recipe: Roti d'épaule érable, moutarde et bière (#888)

---
+++ src/recipes/roti-d-epaule-erable-moutarde-et-biere.ts
@@ -0,0 +1,32 @@
+import { Recipe } from '@/types/recipe';
+
+export const rotiDEpauleErableMoutardeEtBiere: Recipe = {
+  id: 'roti-d-epaule-erable-moutarde-et-biere',
+  title: 'Roti d\'épaule érable, moutarde et bière',
+  description: '',
+  categories: ['Vollaille'],
+  prepTime: 15,
+  cookTime: 480,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '1 roti d\'épaule de porc',
+    '1 c. à soupe de moutarde',
+    '2 c. à soupe de sirop d\'érable',
+    '1 c. à soupe d\'huile d\'olive',
+    '1 gousse d’ail haché',
+    '1/2 bière Herbes de Provence',
+    'Sel',
+    'Poivre'
+  ],
+  instructions: [
+    'Mélangez dans un récipient, moutarde, sirop d\'érable, ail, huile d\'olive, herbes de Provence, sel et poivre.',
+    'Préchauffez le four à 175F.',
+    'Badigeonnez l\'épaule de cette marinade.',
+    'Mettez-la dans une cocotte et ajoutez la bière.',
+    'Cuire environ 8h.',
+    'Coupez le rôti, le mettre sur un plat ou sur une assiette, le nappez de sauce et servir.'
+  ],
+  tags: ['érable', 'marinade sèche', 'rôtisserie'],
+  slug: 'roti-d-epaule-erable-moutarde-et-biere'
+};
