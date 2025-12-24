534d6970b53634af5c9952ee638e6ab4275360c8
David Cloutier <sheldreyn@gmail.com>
2025-12-23 22:32:03 -0500

    Add recipe: Burrata aux tomates

---
+++ src/recipes/burrata-aux-tomates.ts
@@ -0,0 +1,34 @@
+import { Recipe } from '@/types/recipe';
+
+export const burrataAuxTomates: Recipe = {
+  id: 'burrata-aux-tomates',
+  title: 'Burrata aux tomates',
+  description: 'La meilleure des Burrata Burrata aux tomates',
+  categories: ['Entrées', 'Végétarien'],
+  prepTime: 15,
+  cookTime: 0,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '3 tomates',
+    '1/2 botte de basilic',
+    '1/2 botte de persil',
+    '1/2 botte de ciboulette',
+    '3 c. à soupe de vinaigre balsamique',
+    '3 c. à soupe d\'huile d\'olive',
+    'Sel et poivre au goût',
+    '1 burrata d’environ 225 g égouttée',
+    '1 pain baguette tranché'
+  ],
+  instructions: [
+    'Couper les tomates en petits dés et verser dans un saladier.',
+    'Hacher finement les fines herbes et verser dans le saladier.',
+    'Ajouter le vinaigre balsamique et l\'huile au saladier.',
+    'Saler, poivrer et bien brasser.',
+    'Mettre la burrata au centre d\'une grande assiette.',
+    'Verser le mélange de tomates et de fines herbes autour de la burrata.',
+    'Servir avec du pain baguette.'
+  ],
+  tags: ['tomates', 'burrata', 'salade'],
+  slug: 'burrata-aux-tomates'
+};
