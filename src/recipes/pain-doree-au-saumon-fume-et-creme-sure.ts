a31766cf352184dd8afc30937357110c3611df7f
David Cloutier <sheldreyn@gmail.com>
2025-12-23 20:59:08 -0500

    Add recipe: Pain dorée au saumon fumé et crème sure

---
+++ src/recipes/pain-doree-au-saumon-fume-et-creme-sure.ts
@@ -0,0 +1,29 @@
+import { Recipe } from '@/types/recipe';
+
+export const painDoreeAuSaumonFumeEtCremeSure: Recipe = {
+  id: 'pain-doree-au-saumon-fume-et-creme-sure',
+  title: 'Pain dorée au saumon fumé et crème sure',
+  description: 'Pain dorée au saumon fumé et crème sure',
+  categories: ['Vollaille'],
+  prepTime: 15,
+  cookTime: 10,
+  servings: 2,
+  difficulty: 'Facile',
+  ingredients: [
+    '2 gros oeufs (ou 3 petits)',
+    '50 ml de crème 15% champêtre',
+    '1 pincée de fleur de sel',
+    '4 muffins anglais coupés en 2',
+    '1 c. à table de beurre',
+    '6 c. à table de crème sure',
+    '100 g de saumon fumé',
+    'Aneth frais émincé'
+  ],
+  instructions: [
+    'Battez ensemble les oeufs et la crème, ajoutez la fleur de sel et faites imbiber les muffins anglais.',
+    'Dans une poêle antiadhésive chauffée à feu moyen-élevé, faites fondre le beurre, déposez-y les muffins anglais et laissez d’un côté puis de l’autre jusqu’à ce qu’ils aient pris une belle coloration et soient cuits.',
+    'Déposez dans de grandes assiettes, tartinez chaque demi-muffin anglais 1 1/2 c. à table de crème sure, déposez du saumon fumé et décorez d’aneth.'
+  ],
+  tags: ['fumé', 'crème sure', 'grill'],
+  slug: 'pain-doree-au-saumon-fume-et-creme-sure'
+};
