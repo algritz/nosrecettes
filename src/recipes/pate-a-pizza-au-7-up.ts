f5326382b76dbaf81afcf28e206850b69c6c001d
David Cloutier <sheldreyn@gmail.com>
2025-12-24 17:57:42 -0500

    Add recipe: Pâte à pizza au 7 Up (#910)

---
+++ src/recipes/pate-a-pizza-au-7-up.ts
@@ -0,0 +1,37 @@
+import { Recipe } from '@/types/recipe';
+
+export const pateAPizzaAu7Up: Recipe = {
+  id: 'pate-a-pizza-au-7-up',
+  title: 'Pâte à pizza au 7 Up',
+  description: '',
+  categories: ['Plats principaux'],
+  prepTime: 25,
+  cookTime: 20,
+  servings: 2,
+  difficulty: 'Facile',
+  ingredients: [
+    '1 canette de Seven-Up 355 ml à la température de la pièce (important)',
+    '2 1/2 c.à.table (33 ml) d’huile d’olive',
+    '3/4 c.à.thé (4 ml) de sucre',
+    '4 tasses (1000 ml) de farine',
+    '1 c.à.table (15 ml)',
+    '1 1/2 c.à.thé (8 ml) de sel',
+    '2 c.à.thé (10 ml) de levure en pot ou 1 sachet de levure rapide',
+    'œuf'
+  ],
+  instructions: [
+    'Ouvrir le Seven-Up verser dans un plat et saupoudrer le sachet de levure.',
+    'Battre l’œuf avec une fourchette.',
+    'Mélanger le reste des ingrédients secs.',
+    'Mettre l’œuf et l’huile d’olive dans la préparation de levure.',
+    'Mélanger la levure aux ingrédients secs et avec les mains faites une pâte.',
+    'Sur une surface propre, mettre de la farine et pétrir la pâte 2 minutes.',
+    'Huiler un moule et faire monter la pâte recouverte d’un linge propre environ 30 minutes.',
+    'Ensuite couper la pâte en deux et étendre sur des plaques à pizza rondes de 12 pouces ou 30 centimètres.',
+    'Cuisson 400 degré',
+    'Cuisson: 10 à 20 minutes'
+  ],
+  tags: ['pâte à pizza', 'levure', 'Seven-Up'],
+  marinatingTime: 30,
+  slug: 'pate-a-pizza-au-7-up'
+};
