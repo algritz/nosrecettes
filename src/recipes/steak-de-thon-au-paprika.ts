5d8a558760e2bafed10817ae876956661deb9dbf
David Cloutier <sheldreyn@gmail.com>
2025-12-23 20:02:01 -0500

    Add recipe: Steak de thon au paprika

---
+++ src/recipes/steak-de-thon-au-paprika.ts
@@ -0,0 +1,39 @@
+import { Recipe } from '@/types/recipe';
+
+export const steakDeThonAuPaprika: Recipe = {
+  id: 'steak-de-thon-au-paprika',
+  title: 'Steak de thon au paprika',
+  description: '',
+  categories: ['Vollaille'],
+  prepTime: 20,
+  cookTime: 10,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '30 ml de jus de citron',
+    '1 c. à thé d\'herbes de Provence',
+    '200 ml d\'huile d\'olive',
+    '1 c. à thé de paprika',
+    '6 pincées de sel fin',
+    '6 tours de moulin à poivre',
+    '2 à 4 Steaks de thon'
+  ],
+  instructions: [
+    {
+      title: 'Marinade',
+      steps: [
+      'Dans un bol, mélanger à l\'aide d\'une cuillère tous les ingrédients de la marinade.',
+      'Disposer la marinade dans une plaque, puis mettre les steaks de thon pendant une vingtaine de minutes à mariner.'
+      ]
+    },
+    {
+      title: 'Steaks de thon',
+      steps: [
+      'Une fois la braise du barbecue prête, poser les steaks sur la grille et les cuire pendant 8 à 10 min en les retournant à mi-cuisson (veiller à ne pas trop cuire le poisson).',
+      'Vous pouvez badigeonner à l\'aide d\'un pinceau les steaks durant la cuisson avec le reste de marinade.'
+      ]
+    }
+  ],
+  tags: ['barbecue', 'marinade sèche'],
+  slug: 'steak-de-thon-au-paprika'
+};
