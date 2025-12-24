40511abf3b8adfa60c2ef8bdae55e4af1aebb102
David Cloutier <sheldreyn@gmail.com>
2025-12-24 15:52:28 -0500

    Add recipe: Poulet Général Tao d'Isa (#1042)

---
+++ src/recipes/poulet-general-tao-d-isa.ts
@@ -0,0 +1,47 @@
+import { Recipe } from '@/types/recipe';
+
+export const pouletGeneralTaoDIsa: Recipe = {
+  id: 'poulet-general-tao-d-isa',
+  title: 'Poulet Général Tao d\'Isa',
+  description: '',
+  categories: ['Vollaille'],
+  prepTime: 30,
+  cookTime: 30,
+  servings: 2,
+  difficulty: 'Facile',
+  ingredients: [
+    '1 lb de poitrines de poulet désossées, coupées en petits cubes',
+    '2 c. à soupe de fécule de maïs',
+    '1 oeuf battu',
+    '3 c. à soupe de farine',
+    '1/2 c. à thé de poudre à pâte',
+    '1/2 c. à thé de sel',
+    '1/4 de tasse d\'huile végétale',
+    'Graines de sésame au goût',
+    '2 c. à thé d\'huile de sésame',
+    '1/4 de tasse d\'oignons vert coupés en biseau',
+    '1/4 de tasse d\'eau',
+    '1/4 de tasse de vinaigre',
+    '1/4 de tasse de sucre',
+    '1 c. à soupe de fécule de maïs',
+    '1 c. à soupe de sauce aux huîtres',
+    '2 c. à soupe de ketchup',
+    '1 c. à thé de pâte de piments doux'
+  ],
+  instructions: [
+    'Bien enrober les cubes de poulet de fécule de maïs.',
+    'Dans un bol, mélanger l\'œuf, la farine, la poudre à pâte et le sel.',
+    'Ajouter le poulet et s\'assurer que chaque morceau est bien enrobé.',
+    'Dans un grand poêlon antiadhésif, chauffer l\'huile végétale à feu vif.',
+    'Ajouter ensuite le poulet et cuire pendant environ 10 minutes, en remuant constamment, jusqu\'à ce que le poulet soit doré à l\'extérieur et qu\'il perde sa teinte rosée à l\'intérieur.',
+    'Mettre dans une assiette et réserver.',
+    'Dans le même poêlon, ajouter l\'huile de sésame et les oignons verts. Cuire à feu moyen-vif pendant 3 minutes.',
+    'Ajouter l\'eau, le vinaigre et le sucre. Laisser mijoter jusqu\'à ce que le sucre soit complètement dissout.',
+    'Délayer la fécule de maïs dans la sauce soja (important de faire cette étape rapidement pour éviter la formation de grumeaux).',
+    'Ajouter le mélange de sauce soja et fécule, la sauce aux huîtres, le ketchup et la pâte de piments doux. Laisser mijoter pendant 2 minutes ou jusqu\'à ce que la sauce épaississe.',
+    'Remettre le poulet dans le poêlon et mélanger pour réchauffer et bien enrober de sauce.'
+  ],
+  tags: ['général tao', 'sésame', 'piment doux'],
+  notes: 'Dans le Général Tao, il est conseillé de trancher une grande carotte très mince (à la mandoline), de la faire bouillir jusqu\'à ce qu\'elle soit presque cuite mais encore croquante, puis de l\'ajouter dans le poêlon avec le poulet. Il est aussi possible d\'ajouter des piments, mais cela rend la texture un peu molasse.',
+  slug: 'poulet-general-tao-d-isa'
+};
