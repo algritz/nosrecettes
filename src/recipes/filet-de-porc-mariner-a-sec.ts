e27c6156bc46daa84a755837b00d829e3f7185ca
David Cloutier <sheldreyn@gmail.com>
2025-12-23 20:20:42 -0500

    Add recipe: Filet de porc mariner à sec

---
+++ src/recipes/filet-de-porc-mariner-a-sec.ts
@@ -0,0 +1,29 @@
+import { Recipe } from '@/types/recipe';
+
+export const filetDePorcMarinerASec: Recipe = {
+  id: 'filet-de-porc-mariner-a-sec',
+  title: 'Filet de porc mariner à sec',
+  description: '',
+  categories: ['Vollaille', 'Plats principaux'],
+  prepTime: 20,
+  cookTime: 30,
+  servings: 3,
+  difficulty: 'Facile',
+  ingredients: [
+    '1 filet de porc',
+    '[object Object]',
+    '[object Object]'
+  ],
+  instructions: [
+    'Mélanger les ingrédients de la marinade sèche et y faire mariner le porc minimum 30 minutes.',
+    'Faire saisir dans une poêle avec un peu d\'huile.',
+    'Enfourner à 350°F de 20 à 30 minutes à découvert.',
+    'Sortir et laisser reposer 5 à 10 minutes recouvert d\'un papier d\'aluminium.',
+    'Trancher et servir avec la sauce.',
+    'Dans un chaudron, faire chauffer tous les ingrédients de la sauce à feu doux pendant 10 minutes.',
+    'Servir avec le filet de porc.'
+  ],
+  tags: ['marinade sèche', 'grill', 'sauce'],
+  marinatingTime: 30,
+  slug: 'filet-de-porc-mariner-a-sec'
+};
