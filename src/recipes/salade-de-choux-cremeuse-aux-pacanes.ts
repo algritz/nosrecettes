d668aac5e58ae49b3d55c37534291db3b499e358
David Cloutier <sheldreyn@gmail.com>
2025-12-23 19:55:22 -0500

    Add recipe: Salade de choux crémeuse aux pacanes

---
+++ src/recipes/salade-de-choux-cremeuse-aux-pacanes.ts
@@ -0,0 +1,26 @@
+import { Recipe } from '@/types/recipe';
+
+export const saladeDeChouxCremeuseAuxPacanes: Recipe = {
+  id: 'salade-de-choux-cremeuse-aux-pacanes',
+  title: 'Salade de choux crémeuse aux pacanes',
+  description: 'Excellente dans un sandwich de porc effiloché. Salade de choux crémeuse aux pacanes',
+  categories: ['Salades'],
+  prepTime: 15,
+  cookTime: 0,
+  servings: 6,
+  difficulty: 'Facile',
+  ingredients: [
+    'Un demi petit chou blanc, finement haché',
+    'Une petite carotte râpée',
+    '1 oignon vert, finement haché',
+    'Une grosse poignée de pacanes grossièrement hachées',
+    '[object Object]'
+  ],
+  instructions: [
+    'Dans un grand bol, mélanger le chou, l’oignon vert, la carotte et les pécanes.',
+    'Mélanger tous les ingrédients de la vinaigrette, en arroser la salade et bien la répartir partout sur le chou.',
+    'Ajouter du poivre au besoin.'
+  ],
+  tags: ['crémé', 'pécanes', 'vinaigrette'],
+  slug: 'salade-de-choux-cremeuse-aux-pacanes'
+};
