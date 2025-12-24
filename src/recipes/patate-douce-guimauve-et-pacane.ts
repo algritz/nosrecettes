d472ff9b4770c0013c30b92cdb408666032ba60c
David Cloutier <sheldreyn@gmail.com>
2025-12-24 17:42:29 -0500

    Add recipe: Patate douce guimauve et pacane (#928)

---
+++ src/recipes/patate-douce-guimauve-et-pacane.ts
@@ -0,0 +1,31 @@
+import { Recipe } from '@/types/recipe';
+
+export const patateDouceGuimauveEtPacane: Recipe = {
+  id: 'patate-douce-guimauve-et-pacane',
+  title: 'Patate douce guimauve et pacane',
+  description: 'Étonnamment bon !!!',
+  categories: ['Végétarien', 'Accompagnements', 'Plats principaux'],
+  prepTime: 10,
+  cookTime: 20,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '3 à 4 patates douces moyennes, pelées et coupées en rondelles de 1/4 po d\'épaisseur',
+    '2 cuillères à soupe de beurre fondu',
+    '1 c. À thé sirop d\'érable',
+    'Sel casher',
+    '1 sac de guimauve',
+    '1/2 sac de noix de pécan'
+  ],
+  instructions: [
+    'Préchauffer le four à 400°F.',
+    'Sur une grande plaque à pâtisserie, mélanger les patates douces avec le beurre et le sirop d\'érable fondus et les disposer en couche uniforme.',
+    'Assaisonner avec du sel.',
+    'Cuire au four jusqu\'à tendreté, en retournant à mi-cuisson, environ 20 minutes.',
+    'Retirer la plaque à pâtisserie du four et mettre le four à broil.',
+    'Garnir chaque patate douce avec une guimauve et faire griller au four jusqu\'à ce qu\'elle soit gonflée et dorée.',
+    'Garnir immédiatement chaque guimauve avec une moitié de noix de pécan et servir.'
+  ],
+  tags: ['grill', 'sucré', 'noix'],
+  slug: 'patate-douce-guimauve-et-pacane'
+};
