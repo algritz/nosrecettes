1063e85386ba32281293abb0aa54d5fbbb12f782
David Cloutier <sheldreyn@gmail.com>
2025-12-23 22:14:21 -0500

    Add recipe: Guacamole au maïs

---
+++ src/recipes/guacamole-au-mais.ts
@@ -0,0 +1,33 @@
+import { Recipe } from '@/types/recipe';
+
+export const guacamoleAuMais: Recipe = {
+  id: 'guacamole-au-mais',
+  title: 'Guacamole au maïs',
+  description: 'Une recette simple de guacamole avec du maïs grillé, des avocats, et des épices, parfaite pour accompagner vos apéritifs.',
+  categories: ['Entrées'],
+  prepTime: 15,
+  cookTime: 0,
+  servings: 1,
+  difficulty: 'Facile',
+  ingredients: [
+    '3 gros avocats',
+    '2 épis de maïs grillés',
+    '1/2 oignon rouge',
+    '1/2 tasse de coriandre hachée',
+    '3 cuillères à soupe de jus de lime',
+    '1/2 cuillère à café de sel ou au goût',
+    '1/4 cuillère à café de poivre noir ou au goût',
+    '1/2 cuillère à café de cumin'
+  ],
+  instructions: [
+    'Couper les avocats en deux dans le sens de la longueur et retirer les noyaux.',
+    'Utiliser une cuillère pour gratter l\'avocat dans un bol moyen.',
+    'Écraser les avocats pour obtenir une consistance épaisse.',
+    'Presser 3 cuillères à soupe de jus de lime frais sur la purée d\'avocats.',
+    'Couper les grains de maïs des épis et les ajouter au bol avec l\'oignon en dés et la coriandre hachée.',
+    'Assaisonner avec sel, poivre noir et cumin.',
+    'Remuer jusqu\'à ce que le tout soit bien mélangé et servir.'
+  ],
+  tags: ['avocat', 'maïs grillé', 'entrée'],
+  slug: 'guacamole-au-mais'
+};
