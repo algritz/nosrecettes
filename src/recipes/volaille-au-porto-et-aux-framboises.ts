53ec7db45eae2fc74acde65509a40afc6527ee1a
David Cloutier <sheldreyn@gmail.com>
2025-12-23 22:04:40 -0500

    Add recipe: Volaille au porto et aux framboises

---
+++ src/recipes/volaille-au-porto-et-aux-framboises.ts
@@ -0,0 +1,26 @@
+import { Recipe } from '@/types/recipe';
+
+export const volailleAuPortoEtAuxFramboises: Recipe = {
+  id: 'volaille-au-porto-et-aux-framboises',
+  title: 'Volaille au porto et aux framboises',
+  description: '',
+  categories: ['Vollaille', 'Sauces'],
+  prepTime: 10,
+  cookTime: 0,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '4 tournedos de volaille (dinde, poulet ou pintade)',
+    'Huile',
+    '[object Object]',
+    '[object Object]'
+  ],
+  instructions: [
+    'Marinade: Mélanger tous les ingrédients.',
+    'Sauce aux framboises: Dans une casserole, mélanger le porto, la cassonade, la moutarde et les framboises. Faire cuire 10 min. à feu moyen. Passer au mélangeur et réserver la sauce aux framboises.',
+    'Volailles: Verser la marinade sur les tournedos et faire mariner de 4 à 6 h.',
+    'Faire cuire les tournedos sur le barbecue ou griller dans un poêlon.'
+  ],
+  tags: ['barbecue', 'marinade sèche', 'framboise'],
+  slug: 'volaille-au-porto-et-aux-framboises'
+};
