cc86f24f01761c451a663dbc8a2bf2340a655a6c
David Cloutier <sheldreyn@gmail.com>
2025-12-24 17:33:38 -0500

    Add recipe: Tartare de thon fraise et sésame (#937)

---
+++ src/recipes/tartare-de-thon-fraise-et-sesame.ts
@@ -0,0 +1,32 @@
+import { Recipe } from '@/types/recipe';
+
+export const tartareDeThonFraiseEtSesame: Recipe = {
+  id: 'tartare-de-thon-fraise-et-sesame',
+  title: 'Tartare de thon fraise et sésame',
+  description: 'Un tartare frais et parfumé associant le thon, la fraise et le sésame, idéal pour une entrée légère.',
+  categories: ['Entrées'],
+  prepTime: 20,
+  cookTime: 0,
+  servings: 2,
+  difficulty: 'Facile',
+  ingredients: [
+    '2 c. à soupe de ciboulette ciselée',
+    '1 c. à soupe de graines de sésame grillées',
+    '1 c. à thé de gingembre fraîchement râpé',
+    '1 c. à soupe d’huile de sésame',
+    '1 c. à soupe d’huile d’olive',
+    '1 c. à soupe de mayonnaise ou plus au goût',
+    '2 c. à thé de sauce soya légère',
+    '1/2 c. à thé de sauce piquante Sriracha ou au goût',
+    'Sel et poivre au goût',
+    '300 g de thon frais de qualité sushi coupé en petits dés',
+    '1/2 tasse de fraises coupées en petits dés'
+  ],
+  instructions: [
+    'Dans un bol, mélanger tous les ingrédients à l’exception du thon et des fraises.',
+    'Ajouter le thon et les fraises et mélanger délicatement.',
+    'Rectifier l’assaisonnement au goût et servir immédiatement.'
+  ],
+  tags: ['frais', 'sésame', 'entrée'],
+  slug: 'tartare-de-thon-fraise-et-sesame'
+};
