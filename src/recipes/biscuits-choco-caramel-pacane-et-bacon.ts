168f51a2785749c5704ac0b0453cd2fca525e0df
David Cloutier <sheldreyn@gmail.com>
2025-12-24 19:23:53 -0500

    Add recipe: Biscuits choco-caramel pacane et bacon (#814)

---
+++ src/recipes/biscuits-choco-caramel-pacane-et-bacon.ts
@@ -0,0 +1,42 @@
+import { Recipe } from '@/types/recipe';
+
+export const biscuitsChocoCaramelPacaneEtBacon: Recipe = {
+  id: 'biscuits-choco-caramel-pacane-et-bacon',
+  title: 'Biscuits choco-caramel pacane et bacon',
+  description: 'Une recette de biscuits gourmands aux pacanes, chocolat, caramel et bacon, parfaits pour une touche sucrée-salée.',
+  categories: ['Pâtisseries et desserts'],
+  prepTime: 25,
+  cookTime: 14,
+  servings: 12,
+  difficulty: 'Facile',
+  ingredients: [
+    '½ tasse de pacanes',
+    '½ tasse de chocolat noir',
+    '½ tasse de beurre à la température ambiante',
+    '¾ tasse de cassonade',
+    '1½ tasse de farine',
+    '½ c. À thé de bicarbonate de soude',
+    '½ c. À thé de poudre à pâte',
+    '14 petits caramels durs (de type Werther’s Original)',
+    '5 tranches de bacon'
+  ],
+  instructions: [
+    'Préchauffer le four à 350 °F (177 °C).',
+    'Faire griller les pacanes environ 5 minutes sur une plaque à pâtisserie. Laisser refroidir.',
+    'Cuire le bacon jusqu’à ce qu’il soit bien doré. Le couper en morceaux. Réserver.',
+    'Hacher grossièrement les noix, le chocolat et les bonbons au caramel et mélanger au bacon refroidi.',
+    'Conserver une petite quantité de ce mélange pour garnir le dessus des biscuits.',
+    'Dans le bol du batteur électrique, battre le beurre et le sucre en crème jusqu’à ce que le mélange gonfle, blanchisse et prenne une consistance soyeuse.',
+    'Ajouter l’œuf et mélanger de nouveau.',
+    'Dans un bol, tamiser la farine, le bicarbonate et la poudre à pâte.',
+    'Ajouter les ingrédients secs aux ingrédients humides, puis ajouter le bacon, les noix, le chocolat et le caramel.',
+    'Mélanger juste assez pour amalgamer.',
+    'Tapisser une plaque de papier parchemin ou d’un tapis de cuisson en silicone.',
+    'Déposer une grosse cuillérée à soupe de pâte sur la plaque pour former des biscuits en prenant soin de bien les espacer.',
+    'Aplatir très légèrement.',
+    'Faire cuire au four 14 minutes.',
+    'Laisser les biscuits refroidir quelques minutes avant de déguster.'
+  ],
+  tags: ['gourmand', 'salé-sucré', 'bacon'],
+  slug: 'biscuits-choco-caramel-pacane-et-bacon'
+};
