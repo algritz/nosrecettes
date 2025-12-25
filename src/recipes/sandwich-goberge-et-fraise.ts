c521ca644346d1f17ae73be6e1fb4a31e642278c
David Cloutier <sheldreyn@gmail.com>
2025-12-24 22:16:24 -0500

    Add recipe: Sandwich goberge et fraise (#621)

---
+++ src/recipes/sandwich-goberge-et-fraise.ts
@@ -0,0 +1,31 @@
+import { Recipe } from '@/types/recipe';
+
+export const sandwichGobergeEtFraise: Recipe = {
+  id: 'sandwich-goberge-et-fraise',
+  title: 'Sandwich goberge et fraise',
+  description: 'Un sandwich frais et savoureux combinant goberge, fraises, avocat et herbes, idéal pour un repas léger ou une entrée.',
+  categories: ['Sandwichs'],
+  prepTime: 20,
+  cookTime: 0,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '4 pain à sandwich moelleux style petit sous-marin',
+    '750 ml (3 tasses) de fraises, équeutées et coupées en quartiers',
+    '400 g (½ paquet) de goberge, coupée en dés',
+    '2 oignons verts, finement hachés',
+    '1 concombre moyen, pelé, épépiné et coupé en dés',
+    '1 avocat mûr, pelé et coupé en dés',
+    '30 ml (2 c. à soupe) de feuilles de coriandre fraîche hachées',
+    'Au goût, huile d\'olive extra vierge',
+    '15-30 ml (1-2 c. à soupe) de jus de lime (environ 1 ou 2 limes)',
+    'Au goût, sel et poivre du moulin'
+  ],
+  instructions: [
+    'Mélanger tous les ingrédients soigneusement dans un saladier.',
+    'Saler et poivrer.',
+    'Servir en sandwich, dans un pain moelleux!'
+  ],
+  tags: ['frais', 'herbes', 'sandwich'],
+  slug: 'sandwich-goberge-et-fraise'
+};
