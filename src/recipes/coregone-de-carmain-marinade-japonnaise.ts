41f6214aab8f324f85436a412aba8503153a1976
David Cloutier <sheldreyn@gmail.com>
2025-12-24 19:32:21 -0500

    Add recipe: Corégone de Carmain (marinade japonnaise) (#804)

---
+++ src/recipes/coregone-de-carmain-marinade-japonnaise.ts
@@ -0,0 +1,29 @@
+import { Recipe } from '@/types/recipe';
+
+export const coregoneDeCarmainMarinadeJaponnaise: Recipe = {
+  id: 'coregone-de-carmain-marinade-japonnaise',
+  title: 'Corégone de Carmain (marinade japonnaise)',
+  description: '',
+  categories: ['Poisson', 'Marinade'],
+  prepTime: 15,
+  cookTime: 0,
+  servings: 0,
+  difficulty: 'Facile',
+  ingredients: [
+    '¾ tasse d\'huile d\'arachide',
+    '¼ tasse de sauce soya',
+    '¼ tasse de miel',
+    '2 c. à table de vinaigre',
+    '2 c. à table d\'échalotes émincées',
+    '½ c. à thé d\'ail',
+    '1/8 c. à thé de gingembre',
+    'Darne de corégone'
+  ],
+  instructions: [
+    'Mélanger le tout.',
+    'Couper le corégone en darnes et faire mariner plusieurs heures avant de cuire sur BBQ'
+  ],
+  tags: ['japonais', 'marinade', 'barbecue'],
+  marinatingTime: 480,
+  slug: 'coregone-de-carmain-marinade-japonnaise'
+};
