ac3cf01c8de9fbe8fe9421f42ea3d035e0d6b495
David Cloutier <sheldreyn@gmail.com>
2025-12-24 21:27:19 -0500

    Add recipe: Poitrines de canard à la poêle avec sauce aux bleuets (#674)

---
+++ src/recipes/poitrines-de-canard-a-la-poele-avec-sauce-aux-bleuets.ts
@@ -0,0 +1,33 @@
+import { Recipe } from '@/types/recipe';
+
+export const poitrinesDeCanardALaPoeleAvecSauceAuxBleuets: Recipe = {
+  id: 'poitrines-de-canard-a-la-poele-avec-sauce-aux-bleuets',
+  title: 'Poitrines de canard à la poêle avec sauce aux bleuets',
+  description: 'Poitrines de canard à la poêle avec sauce aux bleuets',
+  categories: ['Vollaille'],
+  prepTime: 10,
+  cookTime: 20,
+  servings: 2,
+  difficulty: 'Facile',
+  ingredients: [
+    '1 tasse de bleuets surgelés',
+    '1/3 tasse de gelée de pommes',
+    '1/4 tasse de porto',
+    '3 c. à soupe de vinaigrette balsamique',
+    '2 grosses poitrines de canard'
+  ],
+  instructions: [
+    'Chauffer les bleuets, la gelée, le porto et la vinaigrette dans une petite casserole à feu mi-vif de 13 à 15 min, en remuant fréquemment, jusqu\'à ce que le mélange épaississe et prenne la consistance d\'un sirop.',
+    'Retirer du feu et garder au chaud.',
+    'Entre-temps, à l\'aide d\'un couteau bien aiguisé, faire des incisions en biais dans la peau du canard, à des intervalles de 3/4 pouce, en faisant pénétrer le couteau presque jusqu\'à la chair.',
+    'Faire d\'autres incisions dans le sens contraire pour obtenir un quadrillage.',
+    'Faire chauffer une grande poêle à feu moyen.',
+    'Ajouter les poitrines de canard, le côté peau dans le fond de la poêle.',
+    'Cuire 9 min ou jusqu\'à ce que la peau soit dorée et croustillante.',
+    'Retourner les poitrines et faire cuire de l\'autre côté encore 5 min pour une cuisson à point, jusqu\'à ce que leur centre soit légèrement rose (158 ºF).',
+    'Transférer sur une planche à découper et laisser reposer 5 min.',
+    'Couper en tranches et servir accompagné de la sauce aux bleuets encore chaude.'
+  ],
+  tags: ['canard', 'sauce aux bleuets', 'poêle'],
+  slug: 'poitrines-de-canard-a-la-poele-avec-sauce-aux-bleuets'
+};
