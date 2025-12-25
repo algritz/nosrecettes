c241aa0d32bd470e59c35c52f4fdab5d91b3150b
David Cloutier <sheldreyn@gmail.com>
2025-12-24 19:38:01 -0500

    Add recipe: Boulettes de porc et sauce rosée au fromage (#797)

---
+++ src/recipes/boulettes-de-porc-et-sauce-rosee-au-fromage.ts
@@ -0,0 +1,47 @@
+import { Recipe } from '@/types/recipe';
+
+export const boulettesDePorcEtSauceRoseeAuFromage: Recipe = {
+  id: 'boulettes-de-porc-et-sauce-rosee-au-fromage',
+  title: 'Boulettes de porc et sauce rosée au fromage',
+  description: 'Une recette de boulettes de porc savoureuses accompagnées d\'une sauce rosée crémeuse et fromagée, cuites au four pour une texture parfaite.',
+  categories: ['Plats principaux', 'Vollaille'],
+  prepTime: 20,
+  cookTime: 40,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '1 lb de porc haché',
+    '1 œuf',
+    '2 gousses d’ail, dégermées et finement hachées',
+    '⅓ tasse de chapelure italienne',
+    '1 oz de fromage Mozzarella Coaticook, râpé',
+    '¼ tasse de persil italien frais, haché',
+    '1 c. à soupe de pâte de tomate',
+    '½ c. à thé de sel',
+    'Poivre du moulin',
+    '2 c. à soupe d’huile d’olive',
+    '1 c. à soupe de beurre',
+    '1 oignon jaune, finement haché',
+    '1 boîte 796 ml de tomates italiennes broyées',
+    '½ c. à thé de sel',
+    'Poivre du moulin',
+    '1 c. à soupe de basilic frais haché',
+    '¼ tasse de crème à cuisson 35 %',
+    'Mozzarella râpé',
+    'Basilic frais'
+  ],
+  instructions: [
+    'Dans un grand bol, mélanger ensemble tous les ingrédients pour les boulettes, à l’exception de l’huile d’olive. Former des boulettes.',
+    'Dans une grande poêle antiadhésive, à feu moyen, chauffer l’huile d’olive. Placer les boulettes dans la poêle chaude. Dorer les boulettes sur tous les côtés en les retournant souvent. Transférer sur une assiette et réserver.',
+    'Préchauffer le four à 400 ºF.',
+    'Essuyer la poêle à l’aide d’un papier absorbant. Faire fondre le beurre. Cuire l’oignon à feu moyen pendant environ 5 minutes ou jusqu’à ce qu’il soit translucide.',
+    'Ajouter les tomates broyées, le sel, le poivre et le basilic frais. Porter à ébullition. Réduire le feu et laisser mijoter 5 minutes.',
+    'Ajouter la crème et bien mélanger.',
+    'Dans un plat ou une poêle allant au four, transférer la sauce rosée. Répartir les boulettes dans la sauce et mélanger.',
+    'Recouvrir de fromage mozzarella râpé.',
+    'Cuire au four pendant 20 minutes.',
+    'Garnir de basilic frais et servir.'
+  ],
+  tags: ['porc', 'sauce rosée', 'fromage'],
+  slug: 'boulettes-de-porc-et-sauce-rosee-au-fromage'
+};
