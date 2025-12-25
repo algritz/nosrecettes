fd5aa4f3a41dbb4fdc75d01d5a6693e9105b664a
David Cloutier <sheldreyn@gmail.com>
2025-12-24 20:20:24 -0500

    Add recipe: S’mores-gâteau avec coulis de fruits (#751)

---
+++ src/recipes/s-mores-gateau-avec-coulis-de-fruits.ts
@@ -0,0 +1,29 @@
+import { Recipe } from '@/types/recipe';
+
+export const sMoresGateauAvecCoulisDeFruits: Recipe = {
+  id: 's-mores-gateau-avec-coulis-de-fruits',
+  title: 'S’mores-gâteau avec coulis de fruits',
+  description: '',
+  categories: ['Végétarien', 'Desserts'],
+  prepTime: 15,
+  cookTime: 10,
+  servings: 6,
+  difficulty: 'Facile',
+  ingredients: [
+    'Un plat en aluminium pour le BBQ',
+    'Deux tablettes de votre chocolat préféré en petits morceaux ou râpé (au lait, noir, avec noix, caramel, etc.)',
+    'Des petits fruits (par exemple, un casseau de fraises, un de bleuets et un autre de framboises)',
+    'Des guimauves miniatures (1 tasse)',
+    'Un muffin ou un morceau de gâteau en petits morceaux'
+  ],
+  instructions: [
+    'Sur le plat en aluminium, étendez vos petits fruits.',
+    'Ensuite, mettez les morceaux de gâteau ou de muffin (si vous en avez).',
+    'Placez les guimauves un peu partout.',
+    'Finalement, saupoudrez le chocolat râpé ou déposez les morceaux de façon à ce qu’il y en ait un peu partout.',
+    'Mettez le plat sur le BBQ et laissez cuire quelques minutes.',
+    'Lorsque le chocolat sera fondu ou les guimauves seront un peu dorées, retirez du feu et servir.'
+  ],
+  tags: ['barbecue', 'gâteau', 'chocolat'],
+  slug: 's-mores-gateau-avec-coulis-de-fruits'
+};
