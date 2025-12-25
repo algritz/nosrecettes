cc56654460262ecb1950a517b572dbcbf2f8c0d0
David Cloutier <sheldreyn@gmail.com>
2025-12-25 03:14:37 -0500

    Update tites-saucisse-dans-l-bacon-a-la-sauce-vh.ts

--- src/recipes/tites-saucisse-dans-l-bacon-a-la-sauce-vh.ts
+++ src/recipes/tites-saucisse-dans-l-bacon-a-la-sauce-vh.ts
@@ -1,30 +1,33 @@
 import { Recipe } from '@/types/recipe';
 
 export const titesSaucisseDansLBaconALaSauceVh: Recipe = {
-  id: 'tites-saucisse-dans-l-bacon-a-la-sauce-vh',
-  title: 'Tites saucisse dans l\'bacon à la sauce VH',
-  description: 'Un classique de famille Tites saucisse dans l\'bacon à la sauce VH',
-  categories: ['Végétarien'],
-  prepTime: 25,
-  cookTime: 60,
-  servings: 0,
-  difficulty: 'Facile',
-  ingredients: [
-    'Un paquet de saucisse à hot dog',
-    'Un paquet de bacon canadien',
-    'Un pot de sauce pour cuisson à l\'ail forte pour côtes levées VH'
+  "title": "Tites saucisse dans l'bacon à la sauce VH",
+  "description": "Un classique de famille Tites saucisse dans l'bacon à la sauce VH",
+  "categories": [
+    "Végétarien"
   ],
-  instructions: [
-    'Couper chaque saucisse en 4.',
-    'Couper chaque tranche de bacon en 3.',
-    'Rouler une petite tranche de bacon autour de chaque morceau de saucisse et faire tenir avec un cure-dents.',
-    'Mettre le tout dans un plat de pyrex.',
-    'Verser le pot de sauce VH dessus et enfourner 1h à 1h30.'
+  "prepTime": 25,
+  "cookTime": 60,
+  "servings": 0,
+  "ingredients": [
+    "Un paquet de saucisse à hot dog",
+    "Un paquet de bacon canadien",
+    "Un pot de sauce pour cuisson à l'ail forte pour côtes levées VH"
   ],
-  tags: ['bacon', 'saucisse', 'sauce VH'],
-  notes: 'Marinage: N/A
-Accompagnement: N/A
-Vin: Bourgogne (Pinot Noir) 2012
-Source: recette de famille Racicot',
-  slug: 'tites-saucisse-dans-l-bacon-a-la-sauce-vh'
+  "instructions": [
+    "Couper chaque saucisse en 4.",
+    "Couper chaque tranche de bacon en 3.",
+    "Rouler une petite tranche de bacon autour de chaque morceau de saucisse et faire tenir avec un cure-dents.",
+    "Mettre le tout dans un plat de pyrex.",
+    "Verser le pot de sauce VH dessus et enfourner 1h à 1h30."
+  ],
+  "tags": [
+    "bacon",
+    "saucisse",
+    "sauce VH"
+  ],
+  Source: "recette de famille Racicot",
+  "id": "tites-saucisse-dans-l-bacon-a-la-sauce-vh",
+  "slug": "tites-saucisse-dans-l-bacon-a-la-sauce-vh",
+  "difficulty": "Facile"
 };
