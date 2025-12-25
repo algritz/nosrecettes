5153c4a91988bff1f96a059c8670a2f51c663969
David Cloutier <sheldreyn@gmail.com>
2025-12-24 22:11:49 -0500

    Add recipe: Magret de canard grillé aux fruits de saison et au porto (#626)

---
+++ src/recipes/magret-de-canard-grille-aux-fruits-de-saison-et-au-porto.ts
@@ -0,0 +1,38 @@
+import { Recipe } from '@/types/recipe';
+
+export const magretDeCanardGrilleAuxFruitsDeSaisonEtAuPorto: Recipe = {
+  id: 'magret-de-canard-grille-aux-fruits-de-saison-et-au-porto',
+  title: 'Magret de canard grillé aux fruits de saison et au porto',
+  description: '',
+  categories: ['Vollaille', 'Plats principaux'],
+  prepTime: 20,
+  cookTime: 25,
+  servings: 4,
+  difficulty: 'Facile',
+  ingredients: [
+    '2 magrets de canard',
+    '3 gousses d\'ail, coupées en deux',
+    'Sel et poivre du moulin, au goût',
+    '15 ml (1 c. à soupe) d\'huile',
+    '60 ml (¼ tasse) de vinaigre de framboise',
+    '125 ml (½ tasse) de porto',
+    '60 ml (¼ tasse) de sirop d\'érable',
+    '125 ml (½ tasse) de mûres',
+    '125 ml (½ tasse) de bleuets',
+    '125 ml (½ tasse) de framboises'
+  ],
+  instructions: [
+    'Préparer les magrets en les piquant d\'ail et en saupoudrant de sel et poivre et en incisant le gras en diagonale d\'un sens et dans l\'autre en ne coupant que le gras sans toucher la viande.',
+    'Dans une poêle, chauffer l\'huile et saisir les magrets à feu vif de chaque côté.',
+    'Retirer et disposer le canard dans un plat allant au four.',
+    'Cuire à 180 °C (350 °F) pendant environ 15 minutes.',
+    'Entre-temps, déglacer la poêle avec le vinaigre de framboises.',
+    'Ajouter le porto, le sirop d\'érable et les fruits.',
+    'Laisser mijoter quelques minutes à feu moyen jusqu\'à ce que les fruits tombent un peu en libérant leur jus.',
+    'Retirer les magrets du four en les coupant en aiguillettes (fines tranches).',
+    'Dresser dans les assiettes et napper de la sauce aux fruits et au porto.',
+    'Pour épaissir la sauce, délayer de la fécule de maïs dans l\'eau et ajouter selon besoin au sauce.'
+  ],
+  tags: ['fruits de saison', 'porto', 'sauce'],
+  slug: 'magret-de-canard-grille-aux-fruits-de-saison-et-au-porto'
+};
