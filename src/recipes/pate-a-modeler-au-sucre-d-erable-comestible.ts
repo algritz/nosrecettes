01fbc3e66963fe255331768197a86300ebf1c63a
David Cloutier <sheldreyn@gmail.com>
2025-12-24 21:50:29 -0500

    Add recipe: Pâte à modeler au sucre d’érable (comestible) (#649)

---
+++ src/recipes/pate-a-modeler-au-sucre-d-erable-comestible.ts
@@ -0,0 +1,29 @@
+import { Recipe } from '@/types/recipe';
+
+export const pateAModelerAuSucreDErableComestible: Recipe = {
+  id: 'pate-a-modeler-au-sucre-d-erable-comestible',
+  title: 'Pâte à modeler au sucre d’érable (comestible)',
+  description: 'Une pâte à modeler comestible à base de sucre et de sirop d’érable, idéale pour décorer ou jouer.',
+  categories: ['Végétarien'],
+  prepTime: 10,
+  cookTime: 0,
+  servings: 0,
+  difficulty: 'Facile',
+  ingredients: [
+    '454 g de sucre en poudre',
+    '1/4 tasse de sirop d’érable (de table) ou de maïs',
+    '1/2 cuillère à thé de sel',
+    '1/3 tasse de margarine',
+    '1 cuillère à thé de vanille',
+    'Colorant alimentaire (facultatif)'
+  ],
+  instructions: [
+    'Mélanger tous les ingrédients dans un bol jusqu’à ce que le mélange soit homogène.',
+    'Si le mélange est trop collant, simplement ajouter un peu de sucre en poudre.',
+    'Sculpter votre pâte à modeler pour créer différentes formes, animaux ou autres.',
+    'Vous pouvez la déguster par la suite.'
+  ],
+  tags: ['sucre d’érable', 'pâte à modeler', 'décoration'],
+  notes: 'Essayer la même recette mais avec des essences différentes pour remplacer la vanille (banane, orange, amande, etc.). Utiliser cette pâte pour décorer vos desserts favoris.',
+  slug: 'pate-a-modeler-au-sucre-d-erable-comestible'
+};
