cfed868bca79d755080829cd7770d153db203caa
David Cloutier <sheldreyn@gmail.com>
2025-12-27 12:28:08 -0500

    Update recipe: Casserole de pomme de terre façon tacos

--- src/recipes/casserole-de-pomme-de-terre-facon-tacos.ts
+++ src/recipes/casserole-de-pomme-de-terre-facon-tacos.ts
@@ -36,5 +36,13 @@ export const casseroleDePommeDeTerreFaconTacos: Recipe = {
     'Servez chaud, accompagné de crème sûre et de guacamole si désiré.'
   ],
   tags: ['tacos', 'gratin', 'pomme de terre'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/casserole-de-pomme-de-terre-facon-tacos',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/casserole-de-pomme-de-terre-facon-tacos',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/casserole-de-pomme-de-terre-facon-tacos'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'casserole-de-pomme-de-terre-facon-tacos'
 };
