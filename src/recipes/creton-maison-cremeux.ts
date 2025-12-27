4880c7433f8d810c32c6d367077e8047de956f67
David Cloutier <sheldreyn@gmail.com>
2025-12-27 18:05:41 -0500

    Update recipe: Creton maison crémeux (#1367)

--- src/recipes/creton-maison-cremeux.ts
+++ src/recipes/creton-maison-cremeux.ts
@@ -26,5 +26,13 @@ export const cretonMaisonCremeux: Recipe = {
     'Laisser refroidir.'
   ],
   tags: ['crémeux', 'creton', 'ragoût'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/creton-maison-cremeux',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/creton-maison-cremeux',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/creton-maison-cremeux'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'creton-maison-cremeux'
 };
