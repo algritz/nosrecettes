30f71aa04a165dd064a27cc9ecf78d55f38b6271
David Cloutier <sheldreyn@gmail.com>
2025-12-27 17:06:59 -0500

    Update recipe: Salade de brocoli frais et feta (#1422)

--- src/recipes/salade-de-brocoli-frais-et-feta.ts
+++ src/recipes/salade-de-brocoli-frais-et-feta.ts
@@ -27,5 +27,13 @@ export const saladeDeBrocoliFraisEtFeta: Recipe = {
     'Ajouter à la salade et bien mélanger'
   ],
   tags: ['été', 'feta', 'sauce'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/salade-de-brocoli-frais-et-feta',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/salade-de-brocoli-frais-et-feta',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/salade-de-brocoli-frais-et-feta'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'salade-de-brocoli-frais-et-feta'
 };
