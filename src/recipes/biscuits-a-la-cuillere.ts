c38d8645abab96bfe8724259b04de516ae8230f8
David Cloutier <sheldreyn@gmail.com>
2025-12-27 18:32:24 -0500

    Update recipe: Biscuits à la cuillère (#1340)

--- src/recipes/biscuits-a-la-cuillere.ts
+++ src/recipes/biscuits-a-la-cuillere.ts
@@ -30,5 +30,13 @@ export const biscuitsALaCuillere: Recipe = {
     'Garnir à votre goût.'
   ],
   tags: ['pâtisserie', 'cuisson au four', 'garniture'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/biscuits-a-la-cuillere',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/biscuits-a-la-cuillere',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/biscuits-a-la-cuillere'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'biscuits-a-la-cuillere'
 };
