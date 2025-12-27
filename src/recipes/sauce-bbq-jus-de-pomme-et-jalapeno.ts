0a4e743e2463cc1b9cdfe7b75ec8741f9b916ca1
David Cloutier <sheldreyn@gmail.com>
2025-12-27 16:55:27 -0500

    Update recipe: Sauce BBQ jus de pomme et Jalapeno (#1435)

--- src/recipes/sauce-bbq-jus-de-pomme-et-jalapeno.ts
+++ src/recipes/sauce-bbq-jus-de-pomme-et-jalapeno.ts
@@ -27,5 +27,13 @@ export const sauceBbqJusDePommeEtJalapeno: Recipe = {
     'Mettre tout dans un chaudron et faire mijoter 20 minutes.'
   ],
   tags: ['jambon', 'fumée', 'marinade'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/sauce-bbq-jus-de-pomme-et-jalapeno',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/sauce-bbq-jus-de-pomme-et-jalapeno',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/sauce-bbq-jus-de-pomme-et-jalapeno'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'sauce-bbq-jus-de-pomme-et-jalapeno'
 };
