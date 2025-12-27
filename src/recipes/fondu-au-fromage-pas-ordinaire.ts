49604eceaf11d03e2948d57dc9d0a5602dafa297
David Cloutier <sheldreyn@gmail.com>
2025-12-27 12:40:04 -0500

    Update recipe: Fondu au fromage pas ordinaire

--- src/recipes/fondu-au-fromage-pas-ordinaire.ts
+++ src/recipes/fondu-au-fromage-pas-ordinaire.ts
@@ -30,5 +30,13 @@ export const fonduAuFromagePasOrdinaire: Recipe = {
     'Déguster avec du pain frais.'
   ],
   tags: ['fromage', 'fondue', 'italien'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/fondu-au-fromage-pas-ordinaire',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/fondu-au-fromage-pas-ordinaire',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/fondu-au-fromage-pas-ordinaire'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'fondu-au-fromage-pas-ordinaire'
 };
