85ac86fedc5c356135335a34b5238ed15f0a1a2f
David Cloutier <sheldreyn@gmail.com>
2025-12-27 18:20:48 -0500

    Update recipe: Brownies au Nutella (#1352)

--- src/recipes/brownies-au-nutella.ts
+++ src/recipes/brownies-au-nutella.ts
@@ -39,5 +39,13 @@ export const browniesAuNutella: Recipe = {
     'Laisser tiédir complètement sur une grille avant de démouler et de couper en carrés.'
   ],
   tags: ['Nutella', 'glaçage', 'pâtisserie'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/brownies-au-nutella',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/brownies-au-nutella',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/brownies-au-nutella'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'brownies-au-nutella'
 };
