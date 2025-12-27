9543d8f67770a41fcbdb895247d71f173a2af346
David Cloutier <sheldreyn@gmail.com>
2025-12-27 15:38:51 -0500

    Update recipe: Sauce aux arachides

--- src/recipes/sauce-aux-arachides.ts
+++ src/recipes/sauce-aux-arachides.ts
@@ -25,5 +25,13 @@ export const sauceAuxArachides: Recipe = {
     'Vous pouvez servir chaud ou froid.'
   ],
   tags: ['épicé', 'coco', 'asiane'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/sauce-aux-arachides',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/sauce-aux-arachides',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/sauce-aux-arachides'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'sauce-aux-arachides'
 };
