4815a81eb387fe8a99032f760df589e1c14cfdd1
David Cloutier <sheldreyn@gmail.com>
2025-12-27 15:59:21 -0500

    Update recipe: Tartare de saumon aux fraises

--- src/recipes/tartare-de-saumon-aux-fraises.ts
+++ src/recipes/tartare-de-saumon-aux-fraises.ts
@@ -30,5 +30,13 @@ export const tartareDeSaumonAuxFraises: Recipe = {
     'Servir.'
   ],
   tags: ['frais', 'saumon', 'asiatique'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/tartare-de-saumon-aux-fraises',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/tartare-de-saumon-aux-fraises',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/tartare-de-saumon-aux-fraises'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'tartare-de-saumon-aux-fraises'
 };
