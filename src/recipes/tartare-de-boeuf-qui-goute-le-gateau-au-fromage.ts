2bc435fa7cef9b973fbefd5a61f156c1ccaad699
David Cloutier <sheldreyn@gmail.com>
2025-12-27 16:37:47 -0500

    Update recipe: Tartare de boeuf qui goûte le gâteau au fromage (#1451)

--- src/recipes/tartare-de-boeuf-qui-goute-le-gateau-au-fromage.ts
+++ src/recipes/tartare-de-boeuf-qui-goute-le-gateau-au-fromage.ts
@@ -22,7 +22,15 @@ export const tartareDeBoeufQuiGouteLeGateauAuFromage: Recipe = {
     'Tout mélanger délicatement les ingrédients ensemble.'
   ],
   tags: ['fromage', 'tartare', 'olive'],
-  wine: 'https://www.saq.com/fr/14204442',
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/tartare-de-boeuf-qui-goute-le-gateau-au-fromage',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/tartare-de-boeuf-qui-goute-le-gateau-au-fromage',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/tartare-de-boeuf-qui-goute-le-gateau-au-fromage'
+    }
+  ],
   accompaniment: 'Craquelins aux fruits séchés',
+  wine: 'https://www.saq.com/fr/14204442',
+  source: 'David Cloutier',
   slug: 'tartare-de-boeuf-qui-goute-le-gateau-au-fromage'
 };
