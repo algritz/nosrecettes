e537ffe9da61ed9a62130a2704526d41c96e59c9
David Cloutier <sheldreyn@gmail.com>
2025-12-27 12:31:31 -0500

    Update recipe: Crème de carotte et cari

--- src/recipes/creme-de-carotte-et-cari.ts
+++ src/recipes/creme-de-carotte-et-cari.ts
@@ -33,5 +33,13 @@ export const cremeDeCarotteEtCari: Recipe = {
     'Ajouter du poivre au besoin.'
   ],
   tags: ['curry', 'soupe crémeuse', 'carotte'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/creme-de-carotte-et-cari',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/creme-de-carotte-et-cari',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/creme-de-carotte-et-cari'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'creme-de-carotte-et-cari'
 };
