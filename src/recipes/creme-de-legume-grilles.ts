12bf02c8fd7096c874e341697ab01884a5ae517d
David Cloutier <sheldreyn@gmail.com>
2025-12-27 12:33:17 -0500

    Update recipe: Crème de légume grillés

--- src/recipes/creme-de-legume-grilles.ts
+++ src/recipes/creme-de-legume-grilles.ts
@@ -33,5 +33,13 @@ export const cremeDeLegumeGrilles: Recipe = {
     'Passer au pied mélangeur ou au blender.'
   ],
   tags: ['épicé', 'grillé', 'soupe'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/creme-de-legume-grilles',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/creme-de-legume-grilles',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/creme-de-legume-grilles'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'creme-de-legume-grilles'
 };
