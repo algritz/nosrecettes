e18c68f1afafc4d5a9bbab7d5c469da576a27133
David Cloutier <sheldreyn@gmail.com>
2025-12-27 15:42:29 -0500

    Update recipe: Saumon en sauce primavera

--- src/recipes/saumon-en-sauce-primavera.ts
+++ src/recipes/saumon-en-sauce-primavera.ts
@@ -32,5 +32,13 @@ export const saumonEnSaucePrimavera: Recipe = {
     'Ajouter les filets de saumon dans la sauce et laisser mijoter environ 5 min ou jusqu\'à ce que les filets soient bien cuits à l\'intérieur et que la sauce soit à la consistance désirée.'
   ],
   tags: ['crémeux', 'moutarde', 'mijoter'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/saumon-en-sauce-primavera',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/saumon-en-sauce-primavera',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/saumon-en-sauce-primavera'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'saumon-en-sauce-primavera'
 };
