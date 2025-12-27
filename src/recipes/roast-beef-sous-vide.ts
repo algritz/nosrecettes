42f79ccee92fc4ee9bcdd9584db2aa935383e4ec
David Cloutier <sheldreyn@gmail.com>
2025-12-27 15:21:04 -0500

    Update recipe: Roast Beef sous Vide

--- src/recipes/roast-beef-sous-vide.ts
+++ src/recipes/roast-beef-sous-vide.ts
@@ -30,5 +30,13 @@ export const roastBeefSousVide: Recipe = {
     'Servir avec une sauce au jus ou au poivre'
   ],
   tags: ['sous-vide', 'bœuf', 'croûte'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/roast-beef-sous-vide',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/roast-beef-sous-vide',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/roast-beef-sous-vide'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'roast-beef-sous-vide'
 };
