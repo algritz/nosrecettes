e2e140fcd1bfa3ec72a8ad2f216143a9ee28928a
David Cloutier <sheldreyn@gmail.com>
2025-12-27 16:20:37 -0500

    Update recipe: Vinaigrette César maison (#1469)

--- src/recipes/vinaigrette-cesar-maison.ts
+++ src/recipes/vinaigrette-cesar-maison.ts
@@ -28,5 +28,13 @@ export const vinaigretteCesarMaison: Recipe = {
     'Terminer en ajoutant le parmesan, puis assaisonner et bien brasser.'
   ],
   tags: ['vinaigrette', 'césar', 'maison'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/vinaigrette-cesar-maison',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/vinaigrette-cesar-maison',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/vinaigrette-cesar-maison'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'vinaigrette-cesar-maison'
 };
