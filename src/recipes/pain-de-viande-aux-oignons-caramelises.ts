7e848138eee9a4f657a05d25f6ac35b2fb08ca84
David Cloutier <sheldreyn@gmail.com>
2025-12-27 17:40:07 -0500

    Update recipe: Pain de viande aux oignons caramélisés (#1392)

--- src/recipes/pain-de-viande-aux-oignons-caramelises.ts
+++ src/recipes/pain-de-viande-aux-oignons-caramelises.ts
@@ -32,5 +32,13 @@ export const painDeViandeAuxOignonsCaramelises: Recipe = {
     'Service: Coupez le pain de viande en tranches et servez-les nappées de sauce aux oignons chaud.'
   ],
   tags: ['caramélisation', 'bœuf', 'sauce'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/pain-de-viande-aux-oignons-caramelises',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/pain-de-viande-aux-oignons-caramelises',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/pain-de-viande-aux-oignons-caramelises'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'pain-de-viande-aux-oignons-caramelises'
 };
