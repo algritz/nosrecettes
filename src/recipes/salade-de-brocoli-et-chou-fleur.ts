e7005f2e63c6a9a536881633c87aa3c1af3cdbef
David Cloutier <sheldreyn@gmail.com>
2025-12-27 17:08:10 -0500

    Update recipe: Salade de brocoli et chou-fleur (#1421)

--- src/recipes/salade-de-brocoli-et-chou-fleur.ts
+++ src/recipes/salade-de-brocoli-et-chou-fleur.ts
@@ -27,5 +27,13 @@ export const saladeDeBrocoliEtChouFleur: Recipe = {
     'Laissez reposer au réfrigérateur avant de servir.'
   ],
   tags: ['salade', 'fromage', 'bacon'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/salade-de-brocoli-et-chou-fleur',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/salade-de-brocoli-et-chou-fleur',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/salade-de-brocoli-et-chou-fleur'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'salade-de-brocoli-et-chou-fleur'
 };
