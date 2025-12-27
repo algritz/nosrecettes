8085df32d8fa8d0a9e057708e295a93d8409d533
David Cloutier <sheldreyn@gmail.com>
2025-12-27 14:47:34 -0500

    Update recipe: Macaroni au fromage que les enfants aiment

--- src/recipes/macaroni-au-fromage-que-les-enfants-aiment.ts
+++ src/recipes/macaroni-au-fromage-que-les-enfants-aiment.ts
@@ -33,5 +33,13 @@ export const macaroniAuFromageQueLesEnfantsAiment: Recipe = {
     'Bien brasser et servir.'
   ],
   tags: ['fromage', 'bœuf', 'piment'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/macaroni-au-fromage-que-les-enfants-aiment',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/macaroni-au-fromage-que-les-enfants-aiment',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/macaroni-au-fromage-que-les-enfants-aiment'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'macaroni-au-fromage-que-les-enfants-aiment'
 };
