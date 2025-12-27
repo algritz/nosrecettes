0fa1990e5c59e730160d6e2bb1e99243fa117fb3
David Cloutier <sheldreyn@gmail.com>
2025-12-27 18:17:51 -0500

    Update recipe: Carrés aux barres mars (#1356)

--- src/recipes/carres-aux-barres-mars.ts
+++ src/recipes/carres-aux-barres-mars.ts
@@ -23,5 +23,13 @@ export const carresAuxBarresMars: Recipe = {
     'Faire fondre les chocolats blanc au bain-marie et étendre avec parcimonie sur le chocolat noir afin de faire un effet marbré.'
   ],
   tags: ['chocolat', 'marbré', 'Rice Krispies'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/carres-aux-barres-mars',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/carres-aux-barres-mars',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/carres-aux-barres-mars'
+    }
+  ],
+  source: 'David Cloutier',
   slug: 'carres-aux-barres-mars'
 };
