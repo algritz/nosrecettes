a07ccbfe9573697731e5255f592cbfb2e6f26123
David Cloutier <sheldreyn@gmail.com>
2025-12-29 15:11:31 -0500

    Update recipe: Gravlax de bœuf pimenté

---
+++ src/recipes/gravlax-de-buf-pimente.ts
@@ -0,0 +1,33 @@
+import { Recipe } from '@/types/recipe';
+
+export const gravlaxDeBufPimente: Recipe = {
+  id: 'gravlax-de-b-uf-pimente',
+  title: 'Gravlax de bœuf pimenté',
+  description: '',
+  categories: ['Boeuf', 'Entrées'],
+  prepTime: { min: 20, max: 20 },
+  cookTime: { min: 0, max: 0 },
+  marinatingTime: { min: 1440, max: 1440 },
+  servings: 2,
+  difficulty: 'Facile',
+  ingredients: [
+    '1 lb de filet de bœuf',
+    '2 lbs de gros sel de mer',
+    '3 c. à soupe de grains de poivre noir concassés',
+    '3 c. à soupe de basilic haché',
+    '2 piments extra-forts frais jalapenos haché finement',
+    'Le zeste de 1 citron',
+    'Huile d’olive'
+  ],
+  instructions: [
+    'Mélanger le gros sel, le poivre, le basilic, le zeste et les jalapenos',
+    'Dans un plat, étendre une partie de ce mélange, poser le filet de bœuf dessus et couvrir du reste du mélange de gros sel.',
+    'Couvrir et y mettre un poids pour le presser et réfrigérer 24 heures.',
+    'Le lendemain, gratter le surplus du mélange de sel avec une brosse puis rincer à l’eau froide et sécher la viande avec du papier absorbant.',
+    'Trancher finement le filet.',
+    'Servir arrosé d’un trait d’huile d’olive.'
+  ],
+  tags: ['pimenté', 'marinade sèche', 'bœuf'],
+  source: 'David Cloutier',
+  slug: 'gravlax-de-buf-pimente'
+};
