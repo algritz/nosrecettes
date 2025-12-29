e6d53cb1bd4facf93bd2629878c0ac738992e344
David Cloutier <sheldreyn@gmail.com>
2025-12-29 08:02:46 -0500

    Update recipe: Roast Beef sous Vide

--- src/recipes/roast-beef-sous-vide.ts
+++ src/recipes/roast-beef-sous-vide.ts
@@ -4,7 +4,7 @@ export const roastBeefSousVide: Recipe = {
   id: 'roast-beef-sous-vide',
   title: 'Roast Beef sous Vide',
   description: 'Tendre, comme vous ne l\'aurez jamais vu. Roast Beef sous Vide',
-  categories: ['Plats principaux', 'Vollaille'],
+  categories: ['Plats principaux', 'Cuisson sous-vide', 'Boeuf'],
   prepTime: 20,
   cookTime: 1440,
   servings: 4,
