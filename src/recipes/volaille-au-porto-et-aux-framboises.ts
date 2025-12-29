9384b06bd9dacae72fc811b3f893e9a0fdcecaa8
David Cloutier <sheldreyn@gmail.com>
2025-12-29 14:32:17 -0500

    Update recipe: Volaille au porto et aux framboises (#1496)

--- src/recipes/volaille-au-porto-et-aux-framboises.ts
+++ src/recipes/volaille-au-porto-et-aux-framboises.ts
@@ -1,10 +1,10 @@
-import { Recipe } from '@/types/recipe'
+import { Recipe } from '@/types/recipe';
 
 export const volailleAuPortoEtAuxFramboises: Recipe = {
   id: '1766769975605',
   title: 'Volaille au porto et aux framboises',
   description: '',
-  categories: ['Vollaille', 'Sauces'],
+  categories: ['Vollaille'],
   prepTime: { min: 10, max: 10 },
   cookTime: { min: 0, max: 0 },
   servings: 4,
@@ -12,15 +12,18 @@ export const volailleAuPortoEtAuxFramboises: Recipe = {
   ingredients: [
     {
       title: 'Protéines',
-      items: ['4 tournedos de volaille (dinde, poulet ou pintade)', 'Huile'],
+      items: [
+        '4 tournedos de volaille (dinde, poulet ou pintade)',
+        'Huile'
+      ]
     },
     {
       title: 'Marinade',
       items: [
         '125 ml (½ tasse) jus de framboise',
         '125 ml (½ tasse) porto ou vin rouge',
-        'Sel et poivre',
-      ],
+        'Sel et poivre'
+      ]
     },
     {
       title: 'Sauce aux framboises',
@@ -28,17 +31,17 @@ export const volailleAuPortoEtAuxFramboises: Recipe = {
         '125 ml (½ tasse) porto ou vin rouge',
         '75 ml (1/3 tasse) cassonade',
         '7 ml (1 ½ c. thé) moutarde de Dijon',
-        '250 ml (1 tasse) framboises fraîches ou congelées',
-      ],
-    },
+        '250 ml (1 tasse) framboises fraîches ou congelées'
+      ]
+    }
   ],
   instructions: [
     'Marinade: Mélanger tous les ingrédients.',
     'Sauce aux framboises: Dans une casserole, mélanger le porto, la cassonade, la moutarde et les framboises. Faire cuire 10 min. à feu moyen. Passer au mélangeur et réserver la sauce aux framboises.',
     'Volailles: Verser la marinade sur les tournedos et faire mariner de 4 à 6 h.',
-    'Faire cuire les tournedos sur le barbecue ou griller dans un poêlon.',
+    'Faire cuire les tournedos sur le barbecue ou griller dans un poêlon.'
   ],
   tags: ['barbecue', 'marinade sèche', 'framboise'],
   source: 'David Cloutier',
-  slug: 'volaille-au-porto-et-aux-framboises',
-}
+  slug: 'volaille-au-porto-et-aux-framboises'
+};
