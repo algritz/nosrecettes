4347b912a2068d933842e3dd03cf15884704236a
David Cloutier <sheldreyn@gmail.com>
2025-12-29 17:29:54 -0500

    Update recipe: Filet de porc mariner à sec (#1516)

--- src/recipes/filet-de-porc-mariner-a-sec.ts
+++ src/recipes/filet-de-porc-mariner-a-sec.ts
@@ -1,10 +1,10 @@
-import { Recipe } from '@/types/recipe'
+import { Recipe } from '@/types/recipe';
 
 export const filetDePorcMarinerASec: Recipe = {
   id: '1766691691698',
   title: 'Filet de porc mariner à sec',
   description: '',
-  categories: ['Vollaille', 'Plats principaux'],
+  categories: ['Plats principaux', 'Porc'],
   prepTime: { min: 20, max: 20 },
   cookTime: { min: 30, max: 30 },
   marinatingTime: { min: 30, max: 30 },
@@ -13,7 +13,9 @@ export const filetDePorcMarinerASec: Recipe = {
   ingredients: [
     {
       title: 'Protéines',
-      items: ['1 filet de porc'],
+      items: [
+        '1 filet de porc'
+      ]
     },
     {
       title: 'Marinade sèche',
@@ -22,11 +24,11 @@ export const filetDePorcMarinerASec: Recipe = {
         '30 ml de sucre',
         '30 ml de paprika',
         'Poivre au goût',
-        "10 ml de poudre d'ail",
-        "10 ml de poudre d'oignon",
+        '10 ml de poudre d\'ail',
+        '10 ml de poudre d\'oignon',
         '5 ml de poudre de céleri',
-        '5 ml de poivre de cayenne',
-      ],
+        '5 ml de poivre de cayenne'
+      ]
     },
     {
       title: 'Sauce rouge classique',
@@ -37,20 +39,20 @@ export const filetDePorcMarinerASec: Recipe = {
         '15 ml de moutarde en poudre',
         'Poivre au goût',
         '5 ml de la marinade sèche (voir plus haut)',
-        "125 ml d'eau froide",
-      ],
-    },
+        '125 ml d\'eau froide'
+      ]
+    }
   ],
   instructions: [
     'Mélanger les ingrédients de la marinade sèche et y faire mariner le porc minimum 30 minutes.',
-    "Faire saisir dans une poêle avec un peu d'huile.",
+    'Faire saisir dans une poêle avec un peu d\'huile.',
     'Enfourner à 350°F de 20 à 30 minutes à découvert.',
-    "Sortir et laisser reposer 5 à 10 minutes recouvert d'un papier d'aluminium.",
+    'Sortir et laisser reposer 5 à 10 minutes recouvert d\'un papier d\'aluminium.',
     'Trancher et servir avec la sauce.',
     'Dans un chaudron, faire chauffer tous les ingrédients de la sauce à feu doux pendant 10 minutes.',
-    'Servir avec le filet de porc.',
+    'Servir avec le filet de porc.'
   ],
   tags: ['marinade sèche', 'grill', 'sauce'],
   source: 'David Cloutier',
-  slug: 'filet-de-porc-mariner-a-sec',
-}
+  slug: 'filet-de-porc-mariner-a-sec'
+};
