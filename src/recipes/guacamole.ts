4bd00d33d7c9c461ec5f39944952242452123fd7
David Cloutier <sheldreyn@gmail.com>
2025-12-29 14:23:12 -0500

    Update recipe: Guacamole (#1491)

--- src/recipes/guacamole.ts
+++ src/recipes/guacamole.ts
@@ -1,10 +1,10 @@
-import { Recipe } from '@/types/recipe'
+import { Recipe } from '@/types/recipe';
 
 export const guacamole: Recipe = {
   id: '1758826632070',
   title: 'Guacamole',
   description: '',
-  categories: ['Condiments', 'Sauces', 'Trempettes'],
+  categories: ['Condiments', 'Trempettes'],
   prepTime: { min: 15, max: 15 },
   cookTime: { min: 0, max: 0 },
   marinatingTime: { min: 60, max: 60 },
@@ -14,19 +14,19 @@ export const guacamole: Recipe = {
     '3 avocados - pelées, dénoyauté et broyés',
     '1 lime en jus',
     '1 c. Thé de sel',
-    "2 tasse d'oignon coupé en cube",
+    '2 tasse d\'oignon coupé en cube',
     '3 tbps de coriandre fraiche coupé',
     '2 tomates roma coupé en cube',
-    "1 c. Thé d'ail haché",
-    '1 pincé de poivre de cayenne (optionnel)',
+    '1 c. Thé d\'ail haché',
+    '1 pincé de poivre de cayenne (optionnel)'
   ],
   instructions: [
     'Dans un bol de format moyen, broyer les avocados ',
-    "Ajouter le jus d'une lime et le sel, les oignons, la coriandre, les tomates et l'ail et bien mélanger.",
+    'Ajouter le jus d\'une lime et le sel, les oignons, la coriandre, les tomates et l\'ail et bien mélanger.',
     'Ajouter le poivre de cayenne, au goût.',
-    'Réfrigérer 1 heure pour une meilleur saveur ou bien servir immédiatement.',
+    'Réfrigérer 1 heure pour une meilleur saveur ou bien servir immédiatement.'
   ],
   tags: ['végétarien'],
   source: 'Yannick Benoît',
-  slug: 'guacamole',
-}
+  slug: 'guacamole'
+};
