9895915e4451cdb8104f6861a703c3125c6f40fd
David Cloutier <sheldreyn@gmail.com>
2025-12-29 15:12:58 -0500

    Update recipe: Muffins au bacon et fromage

--- src/recipes/muffins-au-bacon-et-fromage.ts
+++ src/recipes/muffins-au-bacon-et-fromage.ts
@@ -1,10 +1,10 @@
-import { Recipe } from '@/types/recipe'
+import { Recipe } from '@/types/recipe';
 
 export const muffinsAuBaconEtFromage: Recipe = {
   id: 'muffins-au-bacon-et-fromage',
   title: 'Muffins au bacon et fromage',
   description: '',
-  categories: ['Végétarien'],
+  categories: ['Pâtisseries et desserts'],
   prepTime: { min: 15, max: 15 },
   cookTime: { min: 20, max: 20 },
   servings: 12,
@@ -16,22 +16,23 @@ export const muffinsAuBaconEtFromage: Recipe = {
     '1 c. à thé de moutarde sèche',
     '3/4 de c. à thé de sel',
     '1 tasse de lait',
-    "3/4 de tasse d'huile végétale",
+    '3/4 de tasse d\'huile végétale',
     '2 oeufs',
     '1 c. à thé de sirop de maïs',
     '1/3 de tasse de bacon cuit émietté',
     '1 tasse de fromage cheddar finement râpé',
-    '1 oignon vert haché finement',
+    '1 oignon vert haché finement'
   ],
   instructions: [
     'Préchauffer le four à 400 °F et déposer dans les moules à muffins des moules en papier.',
     'Tamiser les ingrédients secs dans un grand bol.',
-    "Ajouter l'huile, le lait, les œufs et le sirop de maïs.",
+    'Ajouter l\'huile, le lait, les œufs et le sirop de maïs.',
     'Bien mélanger.',
     'Incorporer le bacon, le fromage et les oignons verts.',
     'Diviser dans les moules à muffins et cuire 20 minutes à 400 °F.',
-    "Saupoudrer d'un peu de fromage au goût pour décorer.",
+    'Saupoudrer d\'un peu de fromage au goût pour décorer.'
   ],
   tags: ['bacon', 'fromage', 'muffins'],
-  slug: 'muffins-au-bacon-et-fromage',
-}
+  source: 'David Cloutier',
+  slug: 'muffins-au-bacon-et-fromage'
+};
