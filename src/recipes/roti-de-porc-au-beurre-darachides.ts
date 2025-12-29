1e1af663135bfdd75e23be200a481f65d26a9683
David Cloutier <sheldreyn@gmail.com>
2025-12-29 17:02:09 -0500

    Modification recette: Rôti de porc au beurre d’arachides (#1539)
    * Remove old recipe file: roti-de-porc-au-beurre-d-arachides.ts
    
    * Update recipe: Rôti de porc au beurre d’arachides

--- src/recipes/roti-de-porc-au-beurre-darachides.ts
+++ src/recipes/roti-de-porc-au-beurre-darachides.ts
@@ -1,23 +1,22 @@
-import { Recipe } from '@/types/recipe'
+import { Recipe } from '@/types/recipe';
 
-export const rotiDePorcAuBeurreDArachides: Recipe = {
+export const rotiDePorcAuBeurreDarachides: Recipe = {
   id: 'roti-de-porc-au-beurre-d-arachides',
   title: 'Rôti de porc au beurre d’arachides',
-  description:
-    "Un rôti de porc tendre nappé d'une sauce crémeuse au beurre d’arachides, sauce soja et sambal oelek.",
-  categories: ['Vollaille', 'Plats principaux'],
+  description: 'Un rôti de porc tendre nappé d\'une sauce crémeuse au beurre d’arachides, sauce soja et sambal oelek.',
+  categories: ['Plats principaux', 'Porc'],
   prepTime: { min: 20, max: 20 },
   cookTime: { min: 30, max: 30 },
   servings: 4,
   difficulty: 'Facile',
   ingredients: [
     '1 rôti de longe de porc désossé et dégraissé environ 1 kg (2 lbs)',
-    "1/2 à 3/4 tasse de beurre d’arachides croquant 'crunchy' naturel",
+    '1/2 à 3/4 tasse de beurre d’arachides croquant \'crunchy\' naturel',
     '1/2 tasse de crème à 35%',
     '1 c. à soupe de sauce soya',
     '1 c. à thé de sambal oelek',
     'Quelques gouttes d’huile de sésame (facultatif)',
-    'Sel et poivre du moulin',
+    'Sel et poivre du moulin'
   ],
   instructions: [
     'Déposer le rôti dans une poêle, l’assaisonner et le badigeonner de beurre d’arachides.',
@@ -28,8 +27,9 @@ export const rotiDePorcAuBeurreDArachides: Recipe = {
     'Récupérer le jus de cuisson du rôti en l’ajoutant à la sauce.',
     'Ajouter la sauce soya et le sambal oelek.',
     'Bien mélanger et laisser cuire environ 2 minutes.',
-    'Servir les tranches de rôti nappées de sauce.',
+    'Servir les tranches de rôti nappées de sauce.'
   ],
   tags: ['beurre d’arachides', 'sauce crémeuse', 'porc'],
-  slug: 'roti-de-porc-au-beurre-d-arachides',
-}
+  source: 'David Cloutier',
+  slug: 'roti-de-porc-au-beurre-darachides'
+};
