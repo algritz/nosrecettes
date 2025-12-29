9f15578013de7b91b402e6cacdb7efaf3f013bd0
David Cloutier <sheldreyn@gmail.com>
2025-12-29 17:24:23 -0500

    Update recipe: Filet de porc whisky et érable (#1520)

--- src/recipes/filet-de-porc-whisky-et-erable.ts
+++ src/recipes/filet-de-porc-whisky-et-erable.ts
@@ -1,10 +1,10 @@
-import { Recipe } from '@/types/recipe'
+import { Recipe } from '@/types/recipe';
 
 export const filetDePorcWhiskyEtErable: Recipe = {
   id: 'filet-de-porc-whisky-et-erable',
   title: 'Filet de porc whisky et érable',
   description: '',
-  categories: ['Vollaille', 'Plats principaux'],
+  categories: ['Plats principaux', 'Porc'],
   prepTime: { min: 15, max: 15 },
   cookTime: { min: 25, max: 25 },
   servings: 4,
@@ -16,7 +16,7 @@ export const filetDePorcWhiskyEtErable: Recipe = {
     '30 ml (2 c. à table) de moutarde de Dijon',
     '125 ml (1/2 tasse) de persil italien haché',
     '30 ml (2 c. à table) de cidre ou de jus de pomme',
-    '3 ml (1/2 c. à thé) de piments chili rouges, broyés',
+    '3 ml (1/2 c. à thé) de piments chili rouges, broyés'
   ],
   instructions: [
     'Dans un bol en verre, mélangez le whisky, le sirop d’érable, la moutarde de Dijon, le persil coupé, le vinaigre de cidre et les flocons de piment rouge.',
@@ -25,8 +25,9 @@ export const filetDePorcWhiskyEtErable: Recipe = {
     'Préchauffez le barbecue à puissance élevée et huilez la grille.',
     'Déposez les filets sur la grille et baissez la température du barbecue à feu moyen à doux.',
     'Grillez la viande environ 20-25 minutes en la retournant et en la badigeonnant de marinade régulièrement.',
-    'Trancher et server',
+    'Trancher et server'
   ],
   tags: ['barbecue', 'marinade sèche', 'fumoir'],
-  slug: 'filet-de-porc-whisky-et-erable',
-}
+  source: 'David Cloutier',
+  slug: 'filet-de-porc-whisky-et-erable'
+};
