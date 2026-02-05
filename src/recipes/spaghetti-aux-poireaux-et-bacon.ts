7c6e7bcb76f969c016cc14fe5af5d9390f1765d7
David Cloutier <sheldreyn@gmail.com>
2026-02-05 07:49:54 -0500

    Update recipe: Spaghetti aux poireaux et bacon (#1647)

--- src/recipes/spaghetti-aux-poireaux-et-bacon.ts
+++ src/recipes/spaghetti-aux-poireaux-et-bacon.ts
@@ -1,4 +1,4 @@
-import { Recipe } from '@/types/recipe'
+import { Recipe } from '@/types/recipe';
 
 export const spaghettiAuxPoireauxEtBacon: Recipe = {
   id: 'spaghetti-aux-poireaux-et-bacon',
@@ -18,23 +18,30 @@ export const spaghettiAuxPoireauxEtBacon: Recipe = {
     'Parmesan',
     'Eau de cuisson des pâtes',
     '2 œufs pochés',
-    'Sel et poivre au goût',
+    'Sel et poivre au goût'
   ],
   instructions: [
-    "Faire cuire les pâte dans de l'eau bouillante salée.",
+    'Faire cuire les pâte dans de l\'eau bouillante salée.',
     'Séparer la partie blanche du poireau du vert.',
     'Couper le blanc en fines tranches et mettre de côté.',
-    "Couper le vert sur le sens de la longueur en julienne pour qu'il ressemble à de long spaghetti et bien les laver.",
-    "Plonger les poireaux verts dans de l'eau bouillante 3 minutes et par la suite mettre dans de l'eau glacée pour fixer la chlorophylle.",
+    'Couper le vert sur le sens de la longueur en julienne pour qu\'il ressemble à de long spaghetti et bien les laver.',
+    'Plonger les poireaux verts dans de l\'eau bouillante 3 minutes et par la suite mettre dans de l\'eau glacée pour fixer la chlorophylle.',
     'Couper le bacon en fine tranche et faire revenir dans une casserole avec le beurre.',
-    "Une fois le bacon cuit, ajouter le blanc de poireau et faire revenir jusqu'à ce qu'ils tombent.",
+    'Une fois le bacon cuit, ajouter le blanc de poireau et faire revenir jusqu\'à ce qu\'ils tombent.',
     'Ajouter la crème et bien brasser.',
-    "Ajouter les pâtes cuites et environ une louche d'eau de cuisson des pâtes et bien mélanger.",
+    'Ajouter les pâtes cuites et environ une louche d\'eau de cuisson des pâtes et bien mélanger.',
     'Saler et poivrer.',
-    "Servir dans des bols, garnir de parmesan et d'un œuf poché coulant.",
+    'Servir dans des bols, garnir de parmesan et d\'un œuf poché coulant.'
   ],
   tags: ['pâtes', 'poireaux', 'bacon'],
-  notes:
-    'La prochaine fois au lieu des œufs pochés, je vais mettre une burrata.',
-  slug: 'spaghetti-aux-poireaux-et-bacon',
-}
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/spaghetti_poireaux_et_bacon',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/spaghetti_poireaux_et_bacon',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/spaghetti_poireaux_et_bacon'
+    }
+  ],
+  source: 'David Cloutier',
+  notes: 'La prochaine fois au lieu des œufs pochés, je vais mettre une burrata.',
+  slug: 'spaghetti-aux-poireaux-et-bacon'
+};
