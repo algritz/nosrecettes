3eddeffffc818d7961dc5ca5b63991db5d6b4786
David Cloutier <sheldreyn@gmail.com>
2026-02-05 13:55:24 -0500

    Update recipe: Crème de bettraves (#1697)

--- src/recipes/creme-de-bettraves.ts
+++ src/recipes/creme-de-bettraves.ts
@@ -1,10 +1,9 @@
-import { Recipe } from '@/types/recipe'
+import { Recipe } from '@/types/recipe';
 
 export const cremeDeBettraves: Recipe = {
   id: 'creme-de-bettraves',
   title: 'Crème de bettraves',
-  description:
-    'Une soupe de betteraves crémeuse et parfumée, idéale pour une entrée ou un repas léger.',
+  description: 'Une soupe de betteraves crémeuse et parfumée, idéale pour une entrée ou un repas léger.',
   categories: ['Soupes'],
   prepTime: { min: 15, max: 15 },
   cookTime: { min: 60, max: 60 },
@@ -12,24 +11,32 @@ export const cremeDeBettraves: Recipe = {
   difficulty: 'Facile',
   ingredients: [
     '2 oignons, hachés',
-    "1 gousse d'ail, hachée",
+    '1 gousse d\'ail, hachée',
     '45 ml (3 c. à soupe) de beurre',
     '15 ml (1 c. à soupe) de gingembre frais râpé',
     '1 kg (2 lb) de betteraves, pelées et tranchées',
     '1,2 litre (4 1/2 tasses) de bouillon de poulet',
     'Crème sure ou Yogourt nature (pour servir)',
-    'Ciboulette fraîche ciselée (pour garnir)',
+    'Ciboulette fraîche ciselée (pour garnir)'
   ],
   instructions: [
-    "Dans une casserole, attendrir les oignons et l'ail dans le beurre.",
+    'Dans une casserole, attendrir les oignons et l\'ail dans le beurre.',
     'Ajouter le gingembre et les betteraves.',
     'Cuire 1 minute.',
     'Ajouter le bouillon, couvrir et laisser mijoter environ 1 heure.',
     'Saler et poivrer.',
     'Réduire le mélange en purée au mélangeur.',
-    "Rectifier l'assaisonnement.",
-    'Au moment de servir, garnir de crème sure, de ciboulette.',
+    'Rectifier l\'assaisonnement.',
+    'Au moment de servir, garnir de crème sure, de ciboulette.'
   ],
   tags: ['betteraves', 'soupe', 'crémeuse'],
-  slug: 'creme-de-bettraves',
-}
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/creme_de_bettrave',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/creme_de_bettrave',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/creme_de_bettrave'
+    }
+  ],
+  source: 'David Cloutier',
+  slug: 'creme-de-bettraves'
+};
