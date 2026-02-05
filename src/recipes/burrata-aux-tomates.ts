fcda5f79bc40b532f830943a6a03527d47056a2f
David Cloutier <sheldreyn@gmail.com>
2026-02-05 12:53:15 -0500

    Update recipe: Burrata aux tomates (#1684)

--- src/recipes/burrata-aux-tomates.ts
+++ src/recipes/burrata-aux-tomates.ts
@@ -1,4 +1,4 @@
-import { Recipe } from '@/types/recipe'
+import { Recipe } from '@/types/recipe';
 
 export const burrataAuxTomates: Recipe = {
   id: 'burrata-aux-tomates',
@@ -15,20 +15,28 @@ export const burrataAuxTomates: Recipe = {
     '1/2 botte de persil',
     '1/2 botte de ciboulette',
     '3 c. à soupe de vinaigre balsamique',
-    "3 c. à soupe d'huile d'olive",
+    '3 c. à soupe d\'huile d\'olive',
     'Sel et poivre au goût',
     '1 burrata d’environ 225 g égouttée',
-    '1 pain baguette tranché',
+    '1 pain baguette tranché'
   ],
   instructions: [
     'Couper les tomates en petits dés et verser dans un saladier.',
     'Hacher finement les fines herbes et verser dans le saladier.',
-    "Ajouter le vinaigre balsamique et l'huile au saladier.",
+    'Ajouter le vinaigre balsamique et l\'huile au saladier.',
     'Saler, poivrer et bien brasser.',
-    "Mettre la burrata au centre d'une grande assiette.",
+    'Mettre la burrata au centre d\'une grande assiette.',
     'Verser le mélange de tomates et de fines herbes autour de la burrata.',
-    'Servir avec du pain baguette.',
+    'Servir avec du pain baguette.'
   ],
   tags: ['tomates', 'burrata', 'salade'],
-  slug: 'burrata-aux-tomates',
-}
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/burrata_aux_tomates',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/burrata_aux_tomates',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/burrata_aux_tomates'
+    }
+  ],
+  source: 'David Cloutier',
+  slug: 'burrata-aux-tomates'
+};
