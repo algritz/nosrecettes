c57af32b93fdeda5cd52a02d27a7a2d2b3a35657
David Cloutier <sheldreyn@gmail.com>
2026-02-05 07:29:19 -0500

    Update recipe: Tortue de mémère

--- src/recipes/tortue-de-memere.ts
+++ src/recipes/tortue-de-memere.ts
@@ -1,4 +1,4 @@
-import { Recipe } from '@/types/recipe'
+import { Recipe } from '@/types/recipe';
 
 export const tortueDeMemere: Recipe = {
   id: 'tortue-de-memere',
@@ -12,16 +12,24 @@ export const tortueDeMemere: Recipe = {
   ingredients: [
     '1/2 petite barre de paraffine',
     '2 tasses de chocolat en chipit',
-    "3/4 de tasse de beurre d'arachides",
+    '3/4 de tasse de beurre d\'arachides',
     '2 tasses de sucre en poudre',
-    '6 tasses de Corn Flakes',
+    '6 tasses de Corn Flakes'
   ],
   instructions: [
     'Faire fondre la paraffine et le chocolat.',
     'Ajoutez les autres ingrédients et bien mélanger.',
     'Déposer par petites cuillères sur le papier ciré.',
-    'Mettre au réfrigérateur.',
+    'Mettre au réfrigérateur.'
   ],
   tags: ['chocolat', 'corn flakes', 'réfrigération'],
-  slug: 'tortue-de-memere',
-}
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/tortues_de_memere',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/tortues_de_memere',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/tortues_de_memere'
+    }
+  ],
+  source: 'David Cloutier',
+  slug: 'tortue-de-memere'
+};
