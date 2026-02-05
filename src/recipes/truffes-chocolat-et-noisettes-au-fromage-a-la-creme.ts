61d5599c807d474f9404f1d3868ec85b2a88fead
David Cloutier <sheldreyn@gmail.com>
2026-02-05 08:03:55 -0500

    Update recipe: Truffes chocolat et noisettes au fromage à la crème (#1649)

--- src/recipes/truffes-chocolat-et-noisettes-au-fromage-a-la-creme.ts
+++ src/recipes/truffes-chocolat-et-noisettes-au-fromage-a-la-creme.ts
@@ -1,10 +1,9 @@
-import { Recipe } from '@/types/recipe'
+import { Recipe } from '@/types/recipe';
 
 export const truffesChocolatEtNoisettesAuFromageALaCreme: Recipe = {
   id: 'truffes-chocolat-et-noisettes-au-fromage-a-la-creme',
   title: 'Truffes chocolat et noisettes au fromage à la crème',
-  description:
-    'Une recette de truffes chocolat et noisettes au fromage à la crème, facile à préparer et idéale pour une gourmandise raffinée.',
+  description: 'Une recette de truffes chocolat et noisettes au fromage à la crème, facile à préparer et idéale pour une gourmandise raffinée.',
   categories: ['Pâtisseries et desserts'],
   prepTime: { min: 20, max: 20 },
   cookTime: { min: 15, max: 15 },
@@ -15,18 +14,26 @@ export const truffesChocolatEtNoisettesAuFromageALaCreme: Recipe = {
     '2/3 tasse de chapelure de biscuits graham',
     '1/4 tasse de sucre en poudre',
     '1/4 tasse de tartinade aux noisettes (Nutella)',
-    '250 g de chocolat mi-sucré',
+    '250 g de chocolat mi-sucré'
   ],
   instructions: [
-    "Mélanger au batteur le fromage à la crème dans un grand bol jusqu'à consistance crémeuse.",
+    'Mélanger au batteur le fromage à la crème dans un grand bol jusqu\'à consistance crémeuse.',
     'Ajouter la chapelure de biscuits graham, le sucre, la tartinade aux noisettes et bien mélanger.',
     'Façonner le mélange de fromage à la crème en environ 24 boules, à raison de 1 c. à soupe de mélange par boule.',
     'Mettre sur une plaque à pâtisserie tapissée de papier parchemin.',
     'Congeler 15 min.',
     'Faire fondre le chocolat.',
     'Tremper les boules de fromage à la crème, une à la fois, dans le chocolat fondu, en les retournant pour bien les enrober.',
-    "Réfrigérer 1 h ou jusqu'à fermeté.",
+    'Réfrigérer 1 h ou jusqu\'à fermeté.'
   ],
   tags: ['chocolat', 'noisettes', 'gourmandise'],
-  slug: 'truffes-chocolat-et-noisettes-au-fromage-a-la-creme',
-}
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/truffes_chocolat_et_noisettes',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/truffes_chocolat_et_noisettes',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/truffes_chocolat_et_noisettes'
+    }
+  ],
+  source: 'David Cloutier',
+  slug: 'truffes-chocolat-et-noisettes-au-fromage-a-la-creme'
+};
