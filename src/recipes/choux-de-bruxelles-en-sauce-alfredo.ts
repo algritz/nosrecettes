8d0d08e45a5760a29ef7fa6da99f5dc4b7f1234a
David Cloutier <sheldreyn@gmail.com>
2026-02-05 12:27:19 -0500

    Update recipe: choux de bruxelles en sauce Alfredo (#1693)

--- src/recipes/choux-de-bruxelles-en-sauce-alfredo.ts
+++ src/recipes/choux-de-bruxelles-en-sauce-alfredo.ts
@@ -30,6 +30,13 @@ export const chouxDeBruxellesEnSauceAlfredo: Recipe = {
     'Porter à ébullition puis retirer du feu et servir garni du bacon réservé.'
   ],
   tags: ['crème', 'bacon', 'légumes'],
+  images: [
+    {
+      small: 'https://res.cloudinary.com/nosrecettes/image/upload/w_400,h_300,c_fill,q_auto,f_auto/recipes/choux_bruxelle_sauce_alfredo',
+      medium: 'https://res.cloudinary.com/nosrecettes/image/upload/w_800,h_600,c_fill,q_auto,f_auto/recipes/choux_bruxelle_sauce_alfredo',
+      large: 'https://res.cloudinary.com/nosrecettes/image/upload/w_1200,h_900,c_fill,q_auto,f_auto/recipes/choux_bruxelle_sauce_alfredo'
+    }
+  ],
   source: 'David Cloutier',
   slug: 'choux-de-bruxelles-en-sauce-alfredo'
 };
