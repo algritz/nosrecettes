503c9e1e0859dc8877629918c7d10243098592ba
yerishan <yerishan@gmail.com>
2026-06-19 14:59:04 -0400

    Add recipe: Ma recette de pain au levain

---
+++ src/recipes/ma-recette-de-pain-au-levain.ts
@@ -0,0 +1,22 @@
+import { Recipe } from '@/types/recipe';
+
+export const maRecetteDePainAuLevain: Recipe = {
+  id: '1781895543635',
+  title: 'Ma recette de pain au levain',
+  description: 'Après plusieurs tentative, c\'est la meilleur recette de pain au levain.',
+  categories: ['Pain', 'Déjeuners', 'Pâtisseries et desserts'],
+  prepTime: { min: 30, max: 30 },
+  cookTime: { min: 45, max: 45 },
+  marinatingTime: { min: 720, max: 7920 },
+  servings: 1,
+  difficulty: 'Facile',
+  ingredients: [
+    '125 gr de levain frais'
+  ],
+  instructions: [
+
+  ],
+  tags: [],
+  source: 'yerishan',
+  slug: 'ma-recette-de-pain-au-levain'
+};
